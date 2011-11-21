a82faee2a74c9a5e018e2e16be73d60187a3f9df
Yihui Xie <xie@yihui.name>
2011-11-21 02:23:05 -0600

    test cases for parse_inline()

--- inst/tests/test-parser.R
+++ inst/tests/test-parser.R
@@ -5,3 +5,23 @@ test_that('parsing options', {
     expect_error(parse_params('a,b,c=qwer'))
     expect_identical(parse_params('label=xx,opt=zz'),list(label='xx',opt='zz'))
 })
+
+
+test_that('parsing inline texts', {
+    knit_patterns$set(all_patterns$rnw)
+
+    res =
+        parse_inline(c('aaa \\SweaveOpts{eval=TRUE,dev=png,animate=T}',
+                       'bbb \\SweaveOpts{eval=F,dev=tikz,abc=1,comment=} and \\Sexpr{1+2}',
+                       'another expression \\Sexpr{rnorm(10)}'))
+
+    expect_identical(res$params, list(eval=FALSE, dev='tikz', abc=1L, comment=NA))
+    expect_identical(res$code, c('1+2', 'rnorm(10)'))
+    expect_identical(nchar(res$input), 62L)
+
+    res = parse_inline('\\SweaveOpts{a=1}\\SweaveOpts{b=FALSE}')
+    expect_identical(res$params, list(b=FALSE))
+    expect_identical(nchar(res$input), 0L)
+
+    knit_patterns$restore()
+})
