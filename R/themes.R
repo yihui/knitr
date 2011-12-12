a94bd8c763ab07c9f4e5096f63845a064d448156
Ramnath Vaidyanathan <ramnath.vaidya@gmail.com>
2011-12-12 09:54:49 -0500

    add and export get_code_theme

--- R/themes.R
+++ R/themes.R
@@ -1,20 +1,33 @@
-#' Sets the syntax highlighting theme
+#' Sets code theme to use for syntax highlighting
 #'
 #' @export
 #' @author Ramnath Vaidyanathan
-#  TODO: remove extra line introduced in verbatim environment due to \color.
 #  TODO: change par()$col to foreground colour so that figure displays well.
 set_code_theme <- function(theme){
   theme <- fetch_code_theme(theme)
-  # bg <- col2knit(theme$background, ";")
   hi <- paste(c(theme$highlight, boxes_latex()), collapse = "\n")
   opts_chunk$set(background = theme$background)
   set_header(highlight = hi)
-  #   knit_hooks$set(output = function(x, options) {
-  #     paste('{\\color{fgcolor}\\begin{verbatim}', x, '\\end{verbatim}}', sep = "")
-  #   })
   # par(col = theme$foreground)
   return()
+}
+
+#' Gets details of code theme for syntax highlighting 
+#'
+#' @export
+#' @author Ramnath Vaidyanathan
+get_code_theme <- function(theme = NULL){
+  # TODO: replace with system.file during installation
+  # theme_folder <- system.file(package = 'knitr')
+  theme_folder <- path.expand("~/Desktop/R_Projects/knitr/inst")
+  if (is.null(theme)){
+    message('List of Available Code Themes')
+    theme_files <- list.files(file.path(theme_folder, 'codethemes'), 
+      pattern = "css")
+    return(file_path_sans_ext(basename(theme_files)))
+  } else {
+    fetch_code_theme(theme)
+  }
 }
 
 #' Fetches theme file given theme name
@@ -43,8 +56,6 @@ fetch_code_theme  <- function(theme){
   foreground <- css_out$prompt$color
   
   # write latex highlight header
-  # fg <- col2knit(foreground, ",")
-  # fgheader <- sprintf("\\definecolor{fgcolor}{rgb}{%s}", fg)
   fgheader <- color_def(foreground, 'fgcolor')
   highlight <- c(fgheader, styler_assistant_latex(css_out[-1]))
   
