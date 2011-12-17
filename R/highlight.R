bb08d1ca76fb9c8b8817edcfade5a1a8e9b861e7
Ramnath Vaidyanathan <ramnath.vaidya@gmail.com>
2011-12-16 22:36:46 -0500

    keep only functions needed to convert themes from highlight 3.06

--- R/highlight.R
+++ R/highlight.R
@@ -1,37 +1,49 @@
-#' Fetches path to style file from the highlight package
-style_to_file <- function(style){
-  style_file <- sprintf("%s.style", style)
-  return(system.file('highlight', 'themes', style_file, package = 'highlight'))
+#' Parses theme files from Highlight v3.06 by Andre Simon
+#'
+#' @param theme_name name of the theme to parse
+#' @return a list containing the theme specifications
+#' @keywords internal
+theme_to_list <- function(theme_name){
+  theme <- readLines(sprintf('themes-andre/%s.theme', theme_name))
+  y <- theme[grep("\\{.*\\}", theme)]
+  y <- gsub("[{]", 'list(', y)
+  y <- gsub("^(.*)\\}.*$", '\\1)', y)
+  y <- gsub("[;]", "", y)
+  y <- paste(y, collapse = ",")
+  y <- gsub("true", "TRUE", y, fixed = TRUE)
+  y <- eval(parse(text = paste('list(', y, ')')))
+  names(y)[names(y) == ""] <- paste('Keyword', 1:4, sep = "")
+  return(y)
 }
 
-#' Reads contents of style file and returns it as a list
-file_to_list <- function(style_file){
-  style  <- readLines(style_file)
-  style  <- style[grep("$", style, fixed = TRUE)]
-  style  <- sub("$", "", style, fixed = TRUE)
-  style1 <- lapply(strsplit(style, "=|\\s"), "[", 2)
-  names(style1) <- lapply(strsplit(style, "="), "[", 1)
-  return(style1)
-}
-
-#' Generate css file for a style using a template
-style_to_css <- function(style){
-  style1     <- style %oo% style_to_file %oo% file_to_list
+#' Convert theme files from Highlight v3.06 by Andre Simon to CSS 
+#'
+#' @param theme_name name of the theme to parse
+#' @return NULL
+#' @keywords internal
+theme_to_css <- function(theme_name){
+  theme      <- theme_to_list(theme_name)
   css_folder <- fetch_css_folder()
-  css_file   <- file.path(css_folder, sprintf("%s.css", style))
-  template   <- system.file('themes', 'style_template.css', package = 'knitr')
+  css_file   <- file.path(css_folder, sprintf("%s.css", theme_name))
+  template   <- file.path(css_folder, 'highlight3.brew')
   brew::brew(template, css_file)
-  cat('Theme', style, 'saved to', css_file)
+  cat('Theme', theme_name, 'saved to', css_file)
 }
 
-`%oo%` <- function(x, f){
-  f(x)
-}
 
-#' Generate CSS files for all styles in the highlight package
+#' Generate CSS files for all themes in the highlight3 package
+#'
+#' @noRd
 generate_css <- function(){
-  styles <- list.files(system.file('highlight', 'themes', package = 'highlight'))
-  styles <- tools::file_path_sans_ext(styles)
-  plyr::l_ply(styles, style_to_css, .progress = 'text', .print = TRUE)
+  require(tools)
+  require(plyr)
+  
+  `%oo%` <- function(x, f){
+    f(x)
+  }
+  true <- TRUE
+  themes_folder <- file.path('themes-andre')
+  themes <- themes_folder %oo% list.files %oo% file_path_sans_ext
+  l_ply(themes, failwith(NULL, theme_to_css), .progress = 'text', .print = TRUE)
+  return()
 }
-
