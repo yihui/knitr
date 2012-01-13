157cb6037e26ab9ca15211aa6ce8eab6f82a55d2
Yihui Xie <xie@yihui.name>
2012-01-13 16:53:18 -0600

    rename parse_external to read_chunk and document it

--- R/parser.R
+++ R/parser.R
@@ -145,8 +145,27 @@ print.inline = function(x, ...) {
     cat('\n')
 }
 
-## parse an external R script
-parse_external = function(path) {
+##' Read chunks from an external R script
+##'
+##' Chunks can be put in an external R script, and this function reads
+##' chunks into the current \pkg{knitr} session.
+##'
+##' The \code{ref.label} component in the pattern list
+##' (\code{knit_patterns$get('ref.label')}) defines the format of code
+##' chunks.
+##' @param path the path to the R script
+##' @return Code chunks are read into the current session so that
+##' future chunks can use the R code.
+##' @references \url{http://yihui.github.com/knitr/demo/reference/}
+##' @export
+##' @examples ## the default format
+##'
+##' ## @@knitr my-label
+##' 1+1
+##' lm(y~x, data=data.frame(x=1:10,y=rnorm(10)))
+##'
+##' ## later you can use <<my-label>>= to reference this chunk
+read_chunk = function(path) {
     lines = readLines(path, warn = FALSE)
     lab = knit_patterns$get('ref.label')
     if (!group_pattern(lab)) return()
