fb526f4750dfa0717f64365e322566ac02a7204f
Yihui Xie <xie@yihui.name>
2012-01-17 00:36:39 -0600

    fix abs_path() to is_abs_path() in a test

--- inst/tests/test-utils.R
+++ inst/tests/test-utils.R
@@ -1,11 +1,11 @@
 context('Utils')
 
 test_that('abs_path() is working', {
-    expect_false(abs_path('abc/def'))
+    expect_false(is_abs_path('abc/def'))
     if (.Platform$OS.type == 'windows') {
-        expect_true(abs_path('D:\\abc'))
-        expect_true(abs_path('\\\\netdrive\\somewhere'))
+        expect_true(is_abs_path('D:\\abc'))
+        expect_true(is_abs_path('\\\\netdrive\\somewhere'))
     } else {
-        expect_true(abs_path('/abc/def'))
+        expect_true(is_abs_path('/abc/def'))
     }
 })
