e29a8d6d43faf894c3ef9f8cfe327113c9f16958
Yihui Xie <xie@yihui.name>
2012-01-23 13:36:25 -0600

    add FAQ link to ?knitr

--- R/package.R
+++ R/package.R
@@ -9,7 +9,9 @@
 ##' @name knitr-package
 ##' @aliases knitr
 ##' @author Yihui Xie <\url{http://yihui.name}>
-##' @references \url{http://yihui.github.com/knitr/}
+##' @references Full documentation and demos:
+##' \url{http://yihui.github.com/knitr/}; FAQ's:
+##' \url{https://github.com/yihui/knitr/blob/master/FAQ.md}
 ##' @importFrom evaluate evaluate is.recordedplot is.source is.warning is.error is.message
 ##' @importFrom formatR tidy.source
 ##' @importFrom digest digest
