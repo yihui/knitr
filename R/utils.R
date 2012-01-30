0421602a62fde51661aa081acb2b8378deb9a022
Yihui Xie <xie@yihui.name>
2012-01-30 13:27:30 -0600

    new function run_chunk() as an alternative to chunk reference <<label>>

--- R/utils.R
+++ R/utils.R
@@ -288,3 +288,38 @@ knit2pdf = function(input, output = NULL){
     owd = setwd(dirname(out)); on.exit(setwd(owd))
     texi2pdf(basename(out), clean = TRUE)
 }
+
+##' Run the code in a specified chunk
+##'
+##' We can specify a chunk label and use this function to evaluate the
+##' code in this chunk. It is an alternative to the chunk reference in
+##' Sweave.
+##'
+##' The difference between this type of chunk reference to the chunk
+##' option \code{ref.label} is that the latter can only use the same
+##' code as the reference chunk, and this function makes it possible
+##' to collect several little chunks and run them inside another big
+##' chunk.
+##' @param label the chunk label
+##' @return Values returned by the code in the chunk.
+##' @note Recursion (must be finite, of course) of reference is
+##' allowed, e.g. we may run the code of \samp{chunk2} in
+##' \samp{chunk1}, and \samp{chunk2} also contains a reference to
+##' \samp{chunk3}, then if we run \samp{chunk1}, both the code in
+##' \samp{chunk2} and \samp{chunk3} will be evaluated.
+##' @export
+##' @examples ## In Sweave we use chunk reference like this
+##' # <<a>>=
+##' # 1+1
+##' # @@
+##' # <<b>>=
+##' # <<a>>
+##' # @@
+##'
+##' ## In knitr, we use
+##' # <<b>>=
+##' # run_chunk('a')
+##' # @@
+run_chunk = function(label) {
+    eval(parse(text = knit_code$get(label)), envir = knit_env())
+}
