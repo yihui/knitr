875716297cdaedb69295d283b8d1e674f74f8c79
Yihui Xie <xie@yihui.name>
2012-02-11 14:57:55 -0600

    add a test case for commas

--- inst/tests/test-parser.R
+++ inst/tests/test-parser.R
@@ -4,6 +4,8 @@ test_that('parsing options', {
     expect_identical(parse_params('a,b=T,c=def'), list(label='a',b=TRUE,c='def'))
     expect_error(parse_params('a,b,c=qwer'))
     expect_identical(parse_params('label=xx,opt=zz'),list(label='xx',opt='zz'))
+    expect_identical(parse_params('a,b=2,c=qwer\\,asdf=efg'),
+                     list(label='a', b=2L, c='qwer,asdf=efg'))
 })
 
 
