7eb1e0c34fd80af840186e1db28147a9d80d915a
Yihui Xie <xie@yihui.name>
2012-02-15 21:25:29 -0600

    a script to call LyX to compile the examples automatically

---
+++ inst/examples/lyx-compile.R
@@ -0,0 +1,17 @@
+#!/usr/bin/env Rscript
+
+if (!nzchar(Sys.which('lyx')) || system('lyx -version') != 0L) q()
+
+call_lyx = function(file) {
+  res = sapply(sprintf('lyx -e %s %s', c('knitr', 'r', 'pdf2'), file), 
+               system, USE.NAMES = FALSE)
+  unlink(sub('\\.lyx$', '.R', file))
+  stopifnot(identical(res, integer(3L)))
+}
+
+for (i in list.files(pattern = '\\.lyx$')) {
+  message(i)
+  call_lyx(i)
+  flush.console()
+}
+
