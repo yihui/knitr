081afa6baa3da418a9a7012917e4b3ba01b804bb
Yihui Xie <xie@yihui.name>
2012-02-17 22:06:31 -0600

    add a package option 'cache.extra' for #134

--- R/defaults.R
+++ R/defaults.R
@@ -108,6 +108,7 @@ opts_knit =
                       eval.opts = c('eval', 'echo'),
                       all.patterns = all_patterns, tangle = FALSE,
                       child = FALSE, parent = FALSE,
+                      cache.extra = NULL,
 
                       header = c(highlight = '', tikz = '', framed = ''))
 )
