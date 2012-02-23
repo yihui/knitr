cb868f5f7a79f44dbdee5cb9a4e523bb10b4cc50
Yihui Xie <xie@yihui.name>
2012-02-22 22:38:15 -0600

    convenience functions pat_fun()

--- R/pattern.R
+++ R/pattern.R
@@ -33,6 +33,36 @@ knit_patterns =
 
 ## in LaTeX, may need to put this \newcommand{\rinline}[1]{R output}
 
+## convenience functions
+set_pattern = function(type) {
+    knit_patterns$restore()
+    knit_patterns$set(all_patterns[[type]])
+}
+
+##' Set regular expressions to read input documents
+##'
+##' These are convenience functions to set pre-defined pattern lists
+##' (the syntax to read input documents). The function names are built
+##' from corresponding file extensions, e.g. \code{pat_rnw()} can set
+##' the Sweave syntax to read Rnw documents.
+##' @rdname pat_fun
+##' @return The patterns object \code{\link{knit_patterns}} is
+##' modified as a side effect.
+##' @export
+##' @examples ## see how knit_patterns is modified
+##' knit_patterns$get(); pat_rnw(); knit_patterns$get()
+##'
+##' knit_patterns$restore()  # empty the list
+pat_rnw = function() set_pattern('rnw')
+##' @rdname pat_fun
+##' @export
+pat_brew = function() set_pattern('brew')
+##' @rdname pat_fun
+##' @export
+pat_tex = function() set_pattern('tex')
+##' @rdname pat_fun
+##' @export
+pat_html = function() set_pattern('html')
 
 ## helper functions
 
