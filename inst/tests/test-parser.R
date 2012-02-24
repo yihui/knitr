879dd25304a97dfe60f36b0b3d380e27ae9f550d
Yihui Xie <xie@yihui.name>
2012-02-24 16:31:52 -0600

    re-indent code

--- inst/tests/test-parser.R
+++ inst/tests/test-parser.R
@@ -16,23 +16,23 @@ test_that('parsing options', {
 
 
 test_that('parsing inline texts', {
-    knit_patterns$set(all_patterns$rnw)
-
-    res =
-        parse_inline(c('aaa \\SweaveOpts{eval=TRUE,dev=png,animate=T}',
-                       'bbb \\SweaveOpts{eval=F,dev=tikz,abc=1,comment=} and \\Sexpr{1+2}',
-                       'another expression \\Sexpr{rnorm(10)}'))
-
-    expect_identical(res$params, list(eval=FALSE, dev='tikz', abc=1L, comment=NA))
-    expect_identical(res$code, c('1+2', 'rnorm(10)'))
-    expect_identical(nchar(res$input), 62L)
-
-    res = parse_inline('\\SweaveOpts{a=1}\\SweaveOpts{b=FALSE}')
-    expect_identical(res$params, list(b=FALSE))
-    expect_identical(nchar(res$input), 0L)
-
-    res = parse_inline('blabla \\SweaveInput{abc.Rnw} foobar \\Sexpr{1+1}')
-    expect_identical(res$code, c("knit_child('abc.Rnw')", "1+1"))
-
-    knit_patterns$restore()
+  pat_rnw()
+  
+  res =
+    parse_inline(c('aaa \\SweaveOpts{eval=TRUE,dev=png,animate=T}',
+                   'bbb \\SweaveOpts{eval=F,dev=tikz,abc=1,comment=} and \\Sexpr{1+2}',
+                   'another expression \\Sexpr{rnorm(10)}'))
+  
+  expect_identical(res$params, list(eval=FALSE, dev='tikz', abc=1L, comment=NA))
+  expect_identical(res$code, c('1+2', 'rnorm(10)'))
+  expect_identical(nchar(res$input), 62L)
+  
+  res = parse_inline('\\SweaveOpts{a=1}\\SweaveOpts{b=FALSE}')
+  expect_identical(res$params, list(b=FALSE))
+  expect_identical(nchar(res$input), 0L)
+  
+  res = parse_inline('blabla \\SweaveInput{abc.Rnw} foobar \\Sexpr{1+1}')
+  expect_identical(res$code, c("knit_child('abc.Rnw')", "1+1"))
+  
+  knit_patterns$restore()
 })
