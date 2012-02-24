b2135523d73bf517650a0436dc2d45f9ac4a8dd8
Yihui Xie <xie@yihui.name>
2012-02-24 16:31:43 -0600

    test case for new chunk option syntax

--- inst/tests/test-parser.R
+++ inst/tests/test-parser.R
@@ -1,11 +1,17 @@
 context('Parser')
 
 test_that('parsing options', {
-    expect_identical(parse_params('a,b=T,c=def'), list(label='a',b=TRUE,c='def'))
-    expect_error(parse_params('a,b,c=qwer'))
-    expect_identical(parse_params('label=xx,opt=zz'),list(label='xx',opt='zz'))
-    expect_identical(parse_params('a,b=2,c=qwer\\,asdf=efg'),
-                     list(label='a', b=2L, c='qwer,asdf=efg'))
+  ## new syntax
+  expect_identical(parse_params('a-s-d,b=TRUE,c=def'), alist(label='a-s-d',b=TRUE,c=def))
+  expect_error(parse_params('a,b,c=qwer'))
+  expect_error(parse_params('a,c=qwer', label=FALSE))
+  expect_identical(parse_params('a,opt=c(1,3,5)'),alist(label='a',opt=c(1,3,5)))
+  expect_identical(parse_params('label="xx",opt=zz'),alist(label='xx',opt=zz))
+  expect_identical(parse_params('a,b=2,c="qwer",asdf="efg"'),
+                   alist(label='a', b=2, c='qwer',asdf='efg'))
+  ## back-compatibility with Sweave
+  expect_identical(parse_params('abc,fig.path=foo/bar-'),
+                   list(label='abc',fig.path='foo/bar-'))
 })
 
 
