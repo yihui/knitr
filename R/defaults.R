f02a38941ede2e75860ac3e574a0e91b0681dcc3
Yihui Xie <xie@yihui.name>
2012-02-24 23:30:10 -0600

    re-indent code in RStudio

--- R/defaults.R
+++ R/defaults.R
@@ -1,26 +1,26 @@
 new_defaults = function(value = list()) {
-    defaults = value
-
-    get = function(name, default = FALSE) {
-        if (default) defaults = value  # this is only a local version
-        if (missing(name)) defaults else {
-            if (length(name) == 1) defaults[[name]] else defaults[name]
-        }
+  defaults = value
+  
+  get = function(name, default = FALSE) {
+    if (default) defaults = value  # this is only a local version
+    if (missing(name)) defaults else {
+      if (length(name) == 1) defaults[[name]] else defaults[name]
     }
-    set = function(...) {
-        dots = list(...)
-        if (length(dots) == 0) return()
-        if (length(dots) == 1 && is.list(dots[[1]]))
-            dots = dots[[1]]
-        defaults <<- merge(dots)
-    }
-    merge = function(values) {
-        defaults[names(values)] = values
-        defaults
-    }
-    restore = function() defaults <<- value
-
-    list(get = get, set = set, merge = merge, restore = restore)
+  }
+  set = function(...) {
+    dots = list(...)
+    if (length(dots) == 0) return()
+    if (length(dots) == 1 && is.list(dots[[1]]))
+      dots = dots[[1]]
+    defaults <<- merge(dots)
+  }
+  merge = function(values) {
+    defaults[names(values)] = values
+    defaults
+  }
+  restore = function() defaults <<- value
+  
+  list(get = get, set = set, merge = merge, restore = restore)
 }
 
 ##' Default and current chunk options
@@ -35,22 +35,22 @@ new_defaults = function(value = list()) {
 ##' A list of available options: \url{http://yihui.github.com/knitr/options#chunk_options}
 ##' @export
 ##' @examples opts_chunk$get('prompt'); opts_chunk$get('fig.keep')
-opts_chunk =
-    new_defaults(list(eval = TRUE, echo = TRUE, results = 'markup', tidy = TRUE,
-                      cache = FALSE, dependson = NULL, cache.path = 'cache/',
-                      ref.label = NULL, child = NULL,
-                      prompt = FALSE, comment = '##',
-                      fig.keep = 'high', fig.show = 'asis', fig.align = 'default',
-                      fig.path = '', fig.ext = NULL, dev = 'pdf', dpi = 72,
-                      fig.width = 7, fig.height = 7,
-                      fig.cap = NULL, fig.scap = NULL, fig.lp = 'fig:', fig.pos = '',
-                      out.width = NULL, out.height = NULL,
-                      resize.width = NULL, resize.height = NULL,
-                      external = TRUE, sanitize = FALSE,
-                      highlight = TRUE, size = 'normalsize',
-                      warning = TRUE, error = TRUE, message = TRUE,
-                      background = '.97;.97;.97', split = FALSE, include = TRUE,
-                      interval = 1, aniopts = 'controls;loop'))
+opts_chunk = 
+  new_defaults(list(eval = TRUE, echo = TRUE, results = 'markup', tidy = TRUE,
+                    cache = FALSE, dependson = NULL, cache.path = 'cache/',
+                    ref.label = NULL, child = NULL,
+                    prompt = FALSE, comment = '##',
+                    fig.keep = 'high', fig.show = 'asis', fig.align = 'default',
+                    fig.path = '', fig.ext = NULL, dev = 'pdf', dpi = 72,
+                    fig.width = 7, fig.height = 7,
+                    fig.cap = NULL, fig.scap = NULL, fig.lp = 'fig:', fig.pos = '',
+                    out.width = NULL, out.height = NULL,
+                    resize.width = NULL, resize.height = NULL,
+                    external = TRUE, sanitize = FALSE,
+                    highlight = TRUE, size = 'normalsize',
+                    warning = TRUE, error = TRUE, message = TRUE,
+                    background = '.97;.97;.97', split = FALSE, include = TRUE,
+                    interval = 1, aniopts = 'controls;loop'))
 
 ##' @rdname opts_chunk
 ##' @export
@@ -65,32 +65,32 @@ opts_current = new_defaults()
 ##'
 ##' str(all_patterns)
 all_patterns =
-
-    list(`rnw` = list(chunk.begin = '^<<(.*)>>=', chunk.end = '^@\\s*%*',
-         inline.code = '\\\\Sexpr\\{([^}]*)\\}',
-         input.doc = '(^|\n) *\\\\SweaveInput\\{([^}]*)\\}',
-         ref.chunk = '^<<(.*)>>\\s*$',
-         global.options = '\\\\SweaveOpts\\{([^}]*)\\}',
-         header.begin = '\n*\\s*\\\\documentclass[^}]+\\}',
-         document.begin = '\n*\\s*\\\\begin\\{document\\}',
-         ref.label = '^## @knitr (.*)$'),
-
-         `brew` = list(inline.code = '<%[=]{0,1}\\s+([^%]*)\\s+[-]*%>'),
-
-         `tex` = list(chunk.begin = '^%+\\s*begin.rcode\\s*(.*)',
-         chunk.end = '^%+\\s*end.rcode', chunk.code = '^%+',
-         global.options = '%+\\s*roptions\\s*([^\n]*)',
-         inline.code = '\\\\rinline\\{([^}]*)\\}',
-         header.begin = '\n*\\s*\\\\documentclass[^}]+\\}',
-         document.begin = '\n*\\s*\\\\begin\\{document\\}',
-         ref.label = '^## @knitr (.*)$'),
-
-         `html` = list(chunk.begin = '^<!--\\s*begin.rcode\\s*(.*)',
-         chunk.end = '^\\s*end.rcode\\s*-->',
-         inline.code = '<!--\\s*rinline\\s*([^>]*)\\s*-->',
-         global.options = '<!--\\s*roptions\\s*([^>]*)\\s*-->',
-         header.begin = '\n*\\s*<head>',
-         ref.label = '^## @knitr (.*)$'))
+  
+  list(`rnw` = list(chunk.begin = '^<<(.*)>>=', chunk.end = '^@\\s*%*',
+                    inline.code = '\\\\Sexpr\\{([^}]*)\\}',
+                    input.doc = '(^|\n) *\\\\SweaveInput\\{([^}]*)\\}',
+                    ref.chunk = '^<<(.*)>>\\s*$',
+                    global.options = '\\\\SweaveOpts\\{([^}]*)\\}',
+                    header.begin = '\n*\\s*\\\\documentclass[^}]+\\}',
+                    document.begin = '\n*\\s*\\\\begin\\{document\\}',
+                    ref.label = '^## @knitr (.*)$'),
+       
+       `brew` = list(inline.code = '<%[=]{0,1}\\s+([^%]*)\\s+[-]*%>'),
+       
+       `tex` = list(chunk.begin = '^%+\\s*begin.rcode\\s*(.*)',
+                    chunk.end = '^%+\\s*end.rcode', chunk.code = '^%+',
+                    global.options = '%+\\s*roptions\\s*([^\n]*)',
+                    inline.code = '\\\\rinline\\{([^}]*)\\}',
+                    header.begin = '\n*\\s*\\\\documentclass[^}]+\\}',
+                    document.begin = '\n*\\s*\\\\begin\\{document\\}',
+                    ref.label = '^## @knitr (.*)$'),
+       
+       `html` = list(chunk.begin = '^<!--\\s*begin.rcode\\s*(.*)',
+                     chunk.end = '^\\s*end.rcode\\s*-->',
+                     inline.code = '<!--\\s*rinline\\s*([^>]*)\\s*-->',
+                     global.options = '<!--\\s*roptions\\s*([^>]*)\\s*-->',
+                     header.begin = '\n*\\s*<head>',
+                     ref.label = '^## @knitr (.*)$'))
 
 ##' Options for the knitr package
 ##'
@@ -102,17 +102,17 @@ all_patterns =
 ##' @export
 ##' @examples opts_knit$get('verbose'); opts_knit$set(verbose = TRUE)  # change it
 opts_knit =
-    new_defaults(list(progress = TRUE, verbose = FALSE,
-                      out.format = NULL, child.command = 'input',
-                      base.dir = NULL, base.url = NULL, child.path = '',
-                      imgur.key = '60e9e47cff8483c6dc289a1cd674b40f', upload = FALSE,
-                      eval.after = NULL,
-                      all.patterns = all_patterns, tangle = FALSE,
-                      child = FALSE, parent = FALSE,
-                      cache.extra = NULL, aliases = NULL,
-
-                      header = c(highlight = '', tikz = '', framed = ''))
-)
+  new_defaults(list(progress = TRUE, verbose = FALSE,
+                    out.format = NULL, child.command = 'input',
+                    base.dir = NULL, base.url = NULL, child.path = '',
+                    imgur.key = '60e9e47cff8483c6dc289a1cd674b40f', upload = FALSE,
+                    eval.after = NULL,
+                    all.patterns = all_patterns, tangle = FALSE,
+                    child = FALSE, parent = FALSE,
+                    cache.extra = NULL, aliases = NULL,
+                    
+                    header = c(highlight = '', tikz = '', framed = ''))
+               )
 ## header should not be set by hand unless you know what you are doing
 
 ## tangle: whether I'm in tangle mode; child: whether I'm in child
