35d902db1eee37973fde90bc47450727eaf753d9
Yihui Xie <xie@yihui.name>
2012-04-09 16:33:05 -0500

    use environment variable TEXINPUTS for tikz graphics; fixed #188

--- R/plot.R
+++ R/plot.R
@@ -99,6 +99,9 @@ save_plot = function(plot, name, dev, ext, dpi, options) {
   if (dev == 'tikz' && options$external) {
     unlink(pdf.plot <- str_c(name, '.pdf'))
     owd = setwd(dirname(path))
+    # add old wd to TEXINPUTS (see #188)
+    oti = Sys.getenv('TEXINPUTS'); on.exit(Sys.setenv(TEXINPUTS = oti))
+    Sys.setenv(TEXINPUTS = str_c('.', owd, oti, sep = ':'))
     system(str_c(switch(getOption("tikzDefaultEngine"),
                         pdftex = getOption('tikzLatex'),
                         xetex = getOption("tikzXelatex"),
