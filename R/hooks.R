b9fbcb57471f06c409e5cd2aceda0d21b6a05c21
Yihui Xie <xie@yihui.name>
2012-04-11 11:35:41 -0500

    update doc after render_gfm() is removed

--- R/hooks.R
+++ R/hooks.R
@@ -256,17 +256,17 @@ run_hooks = function(before, options, envir) {
 #' Set output hooks for different output formats
 #' 
 #' These functions set built-in output hooks for LaTeX, HTML and Markdown.
-#' 
-#' There are three variants of markdown documents: ordinary markdown 
-#' (\code{render_markdown()}), GFM (GitHub Flavored Markdown; 
-#' \code{render_gfm()}) and Jekyll (a blogging system on GitHub; 
-#' \code{render_jekyll()}). For LaTeX output, there are three variants as well: 
-#' \pkg{knitr}'s default style (\code{render_latex()}; use the LaTeX 
-#' \pkg{framed} package), Sweave style (\code{render_sweave()}; use 
-#' \file{Sweave.sty}) and listings style (\code{render_listings()}; use LaTeX 
-#' \pkg{listings} package). Default HTML output hooks are set by 
-#' \code{render_html()}.
-#' 
+#'
+#' There are three variants of markdown documents: ordinary markdown
+#' (\code{render_markdown(strict = TRUE)}), extended markdown (e.g. GitHub
+#' Flavored Markdown and pandoc; \code{render_markdown(strict = FALSE)}), and
+#' Jekyll (a blogging system on GitHub; \code{render_jekyll()}). For LaTeX
+#' output, there are three variants as well: \pkg{knitr}'s default style
+#' (\code{render_latex()}; use the LaTeX \pkg{framed} package), Sweave style
+#' (\code{render_sweave()}; use \file{Sweave.sty}) and listings style
+#' (\code{render_listings()}; use LaTeX \pkg{listings} package). Default HTML
+#' output hooks are set by \code{render_html()}.
+#'
 #' These functions can be used before \code{knit()} or in the first chunk of the
 #' input document (ideally this chunk has options \code{include = FALSE} and
 #' \code{cache = FALSE}) so that all the following chunks will be formatted as
