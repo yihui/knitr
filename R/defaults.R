7c1bf37c159f64d2669a9cfa545f5f748c611de4
Yihui Xie <xie@yihui.name>
2012-04-11 11:41:24 -0500

    use simpler syntax for inline R code in markdown

--- R/defaults.R
+++ R/defaults.R
@@ -111,7 +111,7 @@ all_patterns =
        `md` = list(chunk.begin = '^``` \\{r(.*)\\}\\s*$',
                    chunk.end = '^````\\s*$',
                    ref.chunk = '^\\s*<<(.*)>>\\s*$',
-                   inline.code = '`ri\\s+([^`]*)\\s+ir`',
+                   inline.code = '`r\\s+([^`]*)\\s+`',
                    global.options = '`ro\\s+([^`]*)\\s+or`',
                    ref.label = '^## @knitr (.*)$'))
 
