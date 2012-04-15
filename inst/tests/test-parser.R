062dd120ef17e4bf3cd0e6e3698a92d3359aa6f6
Yihui Xie <xie@yihui.name>
2012-04-14 19:57:56 -0500

    update test cases for parse_inline()

--- inst/tests/test-parser.R
+++ inst/tests/test-parser.R
@@ -20,11 +20,11 @@ test_that('parsing inline texts', {
   pat_rnw()
   
   res =
-    parse_inline(c('aaa \\SweaveOpts{eval=TRUE,dev=png,animate=T}',
-                   'bbb \\SweaveOpts{eval=F,dev=tikz,abc=1,comment=} and \\Sexpr{1+2}',
+    parse_inline(c('aaa \\SweaveOpts{eval=TRUE,dev="png",animate=T}',
+                   'bbb \\SweaveOpts{eval=F,dev="tikz",abc=1,comment=NA} and \\Sexpr{1+2}',
                    'another expression \\Sexpr{rnorm(10)}'))
   
-  expect_identical(res$params, list(eval=FALSE, dev='tikz', abc=1L, comment=NA))
+  expect_identical(res$params, alist(eval=F, dev='tikz', abc=1, comment=NA))
   expect_identical(res$code, c('1+2', 'rnorm(10)'))
   expect_identical(nchar(res$input), 62L)
   
