5b65fc241cd0556fe24b35d1e27fc6da2dfc8b63
Yihui Xie <xie@yihui.name>
2012-04-28 23:04:53 -0500

    bug fix: should not run read_chunk() when eval=FALSE in purl()

--- R/block.R
+++ R/block.R
@@ -242,7 +242,7 @@ process_tangle.block = function(x) {
     str_c(unlist(cmds), collapse = '\n')
   } else knit_code$get(label)
   # read external code if exists
-  if (length(code) && str_detect(code, 'read_chunk\\(.+\\)')) {
+  if (!isFALSE(params$eval) && length(code) && str_detect(code, 'read_chunk\\(.+\\)')) {
     eval(parse(text = unlist(str_extract_all(code, 'read_chunk\\(([^)]+)\\)'))))
   }
   label_code(parse_chunk(code), label)
