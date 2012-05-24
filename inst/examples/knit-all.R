e85848c6c2e3464fdc5d6c74154d19b049f32926
Yihui Xie <xie@yihui.name>
2012-05-23 21:18:30 -0500

    opts_knit$set(base.url) might be confusing to beginners, so remove it in Rmd and put it in Rscript -e instead

--- inst/examples/knit-all.R
+++ inst/examples/knit-all.R
@@ -17,7 +17,12 @@ for (i in list.files(pattern = '\\.lyx$')) {
 
 for (i in list.files(pattern = '\\.R(tex|md|html|rst)')) {
   message(i)
-  stopifnot(identical(system(sprintf('knit %s', i)), 0L))
+  cmd = if (i == 'knitr-minimal.Rmd') {
+    sprintf("Rscript -e 'library(knitr);opts_knit$set(base.url=\"https://github.com/yihui/knitr/raw/master/inst/examples/\");opts_chunk$set(fig.path=\"\");knit(\"%s\")'", i)
+  } else {
+    sprintf('knit %s', i)
+  }
+  stopifnot(identical(system(cmd), 0L))
   flush.console()
 }
 
