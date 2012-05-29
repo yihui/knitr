37b7594df9afb85450e4d7f3cfe5293f69930d23
Yihui Xie <xie@yihui.name>
2012-05-28 19:08:56 -0500

    also purge .RData files

--- R/cache.R
+++ R/cache.R
@@ -11,7 +11,7 @@ new_cache = function() {
   }
   
   cache_purge = function(hash) {
-    for (h in hash) unlink(str_c(cache_path(h), c('.rdb', '.rdx')))
+    for (h in hash) unlink(str_c(cache_path(h), c('.rdb', '.rdx', '.RData')))
   }
   
   cache_save = function(keys, outname, hash) {
