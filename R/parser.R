514952fffdd8c5ce175a2e9fd1e0b5ddfe3b98bf
Yihui Xie <xie@yihui.name>
2012-06-20 17:07:18 -0400

    use identical() instead of == because global options do not have the 'label' element

--- R/parser.R
+++ R/parser.R
@@ -91,7 +91,7 @@ parse_params = function(params, label = TRUE) {
     }
     if (label && !is.character(res$label))
       res$label = gsub(' ', '', as.character(as.expression(res$label)))
-    if (res$label == '') res$label = unnamed_chunk()
+    if (identical(res$label, '')) res$label = unnamed_chunk()
     return(res)
   }
   warning('(*) NOTE: I saw options "', params,
