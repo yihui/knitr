dbcb43f7b201565d7f0a9757b5e49fe69da6c56a
Yihui Xie <xie@yihui.name>
2012-07-07 01:20:33 -0400

    removed an extra )

--- R/hooks-html.R
+++ R/hooks-html.R
@@ -91,7 +91,7 @@ hook_scianimator = function(x, options) {
   # write the div and js code here
   id = gsub('[^[:alnum:]]', '_', options$label)
   sharpid = str_c("#", id)
-  sprintf('<div class="scianimator"><div id="%s" style="display: inline-block;"></div></div>\n<script type="text/javascript">\n(function($) { \n$(document).ready(function() { \n$("%s").scianimator({ \n"images": [%s],\n"delay": %s,\n"controls": ["first", "previous", "play", "next", "last", "loop", "speed"], \n}); \n$("%s").scianimator("play"); \n}); \n})(jQuery); \n</script> \n</div>', 
+  sprintf('<div class=\"scianimator\"><div id="%s" style="display: inline-block;"></div></div>\n<script type="text/javascript">\n(function($) { \n$(document).ready(function() { \n$("%s").scianimator({ \n"images": [%s],\n"delay": %s,\n"controls": ["first", "previous", "play", "next", "last", "loop", "speed"], \n}); \n$("%s").scianimator("play"); \n}); \n})(jQuery); \n</script> \n</div>', 
           id, sharpid, fig.paths, options$interval * 1000, sharpid)
 }
 
@@ -119,7 +119,7 @@ hook_r2swf = function(x, options) {
   swfhtml = swf2html(file2swf(files = fig.name, swf.name, interval = options$interval),
                      output = FALSE, fragment = TRUE,  width = w, height = h)
   if(options$fig.align == 'default')  return(swfhtml) 
-  sprintf(paste('<div align = "%s">\n', swfhtml, '\n</div>'), options$fig.align))
+  sprintf(paste('<div align = "%s">\n', swfhtml, '\n</div>'), options$fig.align)
 }
 
 #' @rdname output_hooks
