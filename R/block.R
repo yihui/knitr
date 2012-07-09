0496e15eebd30fd1f3a2adcbfe37fae053cd9dad
Cassio M. M. Pereira <cassiomartini@gmail.com>
2012-07-09 08:49:32 -0300

    replacing loop for call to function merge_list from utils.R

--- R/block.R
+++ R/block.R
@@ -19,11 +19,8 @@ call_block = function(block) {
   params = fix_options(params)  # for compatibility
   
   # expand parameters defined via template
-  if(!is.null(params$opts.label)) {
-    for(name in names(opts_template$get(params$opts.label)))
-      params[name] = opts_template$get(params$opts.label)[name]
-  }
-
+  params = merge_list(params, opts_template$get(params$opts.label))
+    
   opts_current$restore(); opts_current$set(params)  # save current options
   label = ref.label = params$label
   if (!is.null(params$ref.label)) ref.label = sc_split(params$ref.label)
