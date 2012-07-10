5472d7b93c962192c3279f1b2fa8ab165db0fd92
Yihui Xie <xie@yihui.name>
2012-07-10 18:11:01 -0400

    add hook_optipng() to optimize PNG images; closes #272

--- R/hooks-extra.R
+++ R/hooks-extra.R
@@ -24,6 +24,11 @@
 #' \command{convert} in ImageMagick is used to trim the white margins (call
 #' \command{convert input -trim output}).
 #'
+#' The function \code{hook_optipng} calls the program \command{optipng} to
+#' optimize PNG images. Note the chunk option \code{optipng.opts} can be used to
+#' provide additional parameters to \command{optipng}, e.g. \code{optipng.opts =
+#' '-o7'}. See \url{http://optipng.sourceforge.net/} for details.
+#'
 #' When the plots are not recordable via \code{\link[grDevices]{recordPlot}} and
 #' we save the plots to files manually via other functions (e.g. \pkg{rgl}
 #' plots), we can use the chunk hook \code{hook_plot_custom} to help write code
@@ -74,6 +79,28 @@ hook_pdfcrop = function(before, options, envir) {
     message('cropping ', x)
     x = shQuote(x)
     cmd = if (ext == 'pdf') paste("pdfcrop", x, x) else paste('convert', x, '-trim', x)
+    if (.Platform$OS.type == 'windows') cmd = paste(Sys.getenv("COMSPEC"), "/c", cmd)
+    system(cmd)
+  })
+  return()
+}
+#' @export
+#' @rdname chunk_hook
+hook_optipng = function(before, options, envir) {
+  if (before) return()
+  ext = tolower(options$fig.ext)
+  if (ext != 'png') {
+    warning('this hook only works with PNG at the moment'); return()
+  }
+  if (!nzchar(Sys.which('optipng'))) {
+    warning('cannot find optipng; please install and put it in PATH'); return()
+  }
+  paths = all_figs(options, ext)
+
+  lapply(paths, function(x) {
+    message('optimizing ', x)
+    x = shQuote(x)
+    cmd = paste('optipng', options$optpng.opts, x)
     if (.Platform$OS.type == 'windows') cmd = paste(Sys.getenv("COMSPEC"), "/c", cmd)
     system(cmd)
   })
