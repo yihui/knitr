5788505a919101294c3e0d95bdb8c6586f96d891
Yihui Xie <xie@yihui.name>
2012-08-07 23:03:50 -0400

    some notes in ?knitr

--- R/package.R
+++ R/package.R
@@ -1,5 +1,5 @@
 #' A general-purpose tool for dynamic report generation in R
-#' 
+#'
 #' This is an alternative tool to Sweave with more flexible design and new
 #' features like cache and fine control of graphics. It is not limited to LaTeX
 #' and is ready to be customized to process other file formats. See the package
@@ -8,6 +8,11 @@
 #' @name knitr-package
 #' @aliases knitr
 #' @author Yihui Xie <\url{http://yihui.name}>
+#' @seealso The core function in this package: \code{\link{knit}}
+#' @note The pronunciation of \pkg{knitr} is similar to \emph{neater} (neater
+#'   than what?) or you can think of \emph{knitter} (but it is \emph{single t}).
+#'   The name comes from \code{knit} + \code{R} (while \code{Sweave} = \code{S}
+#'   + \code{weave}).
 #' @references Full documentation and demos: \url{http://yihui.name/knitr/};
 #'   FAQ's: \url{https://github.com/yihui/knitr/blob/master/FAQ.md}
 #' @importFrom evaluate evaluate is.recordedplot is.source is.warning is.error
