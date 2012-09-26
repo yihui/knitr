42dc46e71ade2caf7b29e258ea4701325ba44ffa
Yihui Xie <xie@yihui.name>
2012-09-26 00:40:35 -0500

    evaluate will add \n to source lines, which should be removed before passing code to line_prompt()
    then line_prompt() does not have to manipulate \n any more

--- R/output.R
+++ R/output.R
@@ -406,11 +406,10 @@ wrap.character = function(x, options) {
 }
 
 wrap.source = function(x, options) {
-  src = x$src
+  src = str_replace(x$src, '\n$', '')
   if (options$highlight) {
     src = hilight_source(src, out_format(), options)
   } else if (options$prompt) src = line_prompt(src)
-  src = str_replace(src, '\n$', '')
   src = str_c(c(src, ''), collapse = '\n')
   knit_hooks$get('source')(src, options)
 }
