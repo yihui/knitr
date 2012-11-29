032a238bdb0c78fcc4db094cd8e7891471b1c0d3
Yihui Xie <xie@yihui.name>
2012-11-29 14:43:21 -0600

    knit_hooks$get(default = TRUE) is nothing but .default.hooks

--- R/hooks-chunk.R
+++ R/hooks-chunk.R
@@ -1,8 +1,8 @@
 ## hooks that return character values will be inserted into final output
 run_hooks = function(before, options, envir) {
   ## default and user-defined new hooks
-  hooks.d = knit_hooks$get(default = TRUE); hooks.n = knit_hooks$get()
-  hooks.a = hooks.n[setdiff(names(hooks.n), names(hooks.d))] # a list of hooks to run
+  hooks.n = knit_hooks$get()
+  hooks.a = hooks.n[setdiff(names(hooks.n), names(.default.hooks))] # a list of hooks to run
   out = NULL
   for (i in names(hooks.a)) {
     if (!is.null(options[[i]])) {
