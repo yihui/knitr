0945334843f6fd283bac5f0320bf46b5705dec11
Yihui Xie <xie@yihui.name>
2013-02-01 01:30:07 -0600

    for R 3.0.0, we can specify the vignette builder in DESCRIPTION and register the builder functions through tools::vignetteEngine()

--- R/zzz.R
+++ R/zzz.R
@@ -1,4 +1,6 @@
 .onLoad = function(lib, pkg) {
+  if (getRversion() >= '3.0.0')
+    tools::vignetteEngine('knitr', weave = vweave, tangle = vtangle)
   # use the env var R_KNITR_PROGRESS to control the progress bar
   opts_knit$set(progress = isTRUE(as.logical(Sys.getenv('R_KNITR_PROGRESS', TRUE))))
   # add /usr/texbin to PATH for Mac OS
