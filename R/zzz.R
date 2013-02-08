9918614841775b336cd3da9b72ad8be03eb788d6
Yihui Xie <xie@yihui.name>
2013-02-07 18:05:00 -0600

    use the svn revision number instead of R version number

--- R/zzz.R
+++ R/zzz.R
@@ -1,5 +1,5 @@
 .onLoad = function(lib, pkg) {
-  if (getRversion() >= '3.0.0')
+  if (R.version$`svn rev` >= 61843)
     tools::vignetteEngine('knitr', weave = vweave, tangle = vtangle)
   # use the env var R_KNITR_PROGRESS to control the progress bar
   opts_knit$set(progress = isTRUE(as.logical(Sys.getenv('R_KNITR_PROGRESS', TRUE))))
