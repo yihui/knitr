1aee7c76a704977885fa3f1742226bf3f28d863f
Yihui Xie <xie@yihui.name>
2013-03-05 16:17:48 -0600

    use r62130 of r-devel to add vignette engines: now we specify the file patterns and (optionally) the package name

--- R/zzz.R
+++ R/zzz.R
@@ -1,6 +1,10 @@
 .onLoad = function(lib, pkg) {
-  if (R.version$`svn rev` >= 61843 && getRversion() >= '3.0.0')
-    tools::vignetteEngine('knitr', weave = vweave, tangle = vtangle)
+  if (R.version$`svn rev` >= 62130 && getRversion() >= '3.0.0') {
+    tools::vignetteEngine(
+      'knitr', weave = vweave, tangle = vtangle,
+      pattern = c('[.][rRsS](nw|tex)$', '[.][Rr](md|html)$'), package = pkg
+    )
+  }
   # use the option KNITR_PROGRESS to control the progress bar
   opts_knit$set(progress = getOption('KNITR_PROGRESS', TRUE))
   # add /usr/texbin to PATH for Mac OS
