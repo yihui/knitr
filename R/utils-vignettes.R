7f64e30028fc8e80a9a2f102d4e6e39fb106bc63
Yihui Xie <xie@yihui.name>
2013-03-26 15:46:47 -0500

    add quiet to the vignette functions as well

--- R/utils-vignettes.R
+++ R/utils-vignettes.R
@@ -1,8 +1,9 @@
 # Wrappers to use in vignette building for R 3.0.0
 
 vweave = vtangle = function(file, driver, syntax, encoding = '', quiet = FALSE, ...) {
-  if (quiet) opts_knit$set(progress = FALSE)
-  (if (grepl('\\.[Rr]md$', file)) knit2html else knit)(file, encoding = encoding)
+  (if (grepl('\\.[Rr]md$', file)) knit2html else knit)(
+    file, encoding = encoding, quiet = quiet
+  )
 }
 
-body(vtangle)[3L] = expression(purl(file, encoding = encoding))
+body(vtangle)[2L] = expression(purl(file, encoding = encoding, quiet = quiet))
