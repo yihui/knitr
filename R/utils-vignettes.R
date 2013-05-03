7e18c193fe5cef40b005d0e5aa0b24ace8cc3d3d
trevor <trevorld@stanford.edu>
2013-05-03 11:08:39 -0700

    Add rst to vignette engine

--- R/utils-vignettes.R
+++ R/utils-vignettes.R
@@ -2,9 +2,14 @@
 
 vweave = vtangle = function(file, driver, syntax, encoding = '', quiet = FALSE, ...) {
   opts_knit$set(stop_on_error = 2L)  # should not hide errors
-  (if (grepl('\\.[Rr]md$', file)) knit2html else knit)(
-    file, encoding = encoding, quiet = quiet, envir = globalenv()
-  )
+  if(grepl('\\.[Rr]md$', file)) {
+    engine <- knit2html 
+  } else if (grepl('\\.[Rr]rst$', file)) {
+    engine <- knit2pdf
+  } else {
+    engine <- knit
+  }
+  engine(file, encoding = encoding, quiet = quiet, envir = globalenv())
 }
 
 body(vtangle)[3L] = expression(purl(file, encoding = encoding, quiet = quiet))
