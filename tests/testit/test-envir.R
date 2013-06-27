bb9ea5ae383dc30f5b88a06f1766cf5e26bad515
Yihui Xie <xie@yihui.name>
2013-06-26 23:55:11 -0700

    tweak the test for environments

--- tests/testit/test-envir.R
+++ tests/testit/test-envir.R
@@ -2,14 +2,15 @@ library(testit)
 
 rm(list = ls())
 z = 5
-env = new.env()
-
 assert(
   'a list can be used as the parent frame for knit()',
   # evaluate in a new environment; should create an object in current envir
-  !has_error(with(list(y = 4:8), knit('knit-envir.Rmd', envir = env, quiet = TRUE)))
+  !has_error(with(list(y = 4:8), knit('knit-envir.Rmd', quiet = TRUE)))
 )
 
+env = new.env()
+env$y = 1:3
+knit('knit-envir.Rmd', envir = env, quiet = TRUE)
 assert(
   'knit() creates objects in its envir argument',
   !exists('asdfqwerzxcv'), exists('asdfqwerzxcv', envir = env)
@@ -17,8 +18,9 @@ assert(
 
 assert(
   'undefined external objects should cause errors',
-  has_error(knit('knit-envir.Rmd')),  # y is not found
-  !has_error(with(list(y = letters), knit('knit-envir.Rmd')))
+  has_error(knit('knit-envir.Rmd', quiet = TRUE)),  # y is not found
+  !has_error(with(list(y = letters), knit('knit-envir.Rmd', quiet = TRUE)))
 )
 
+file.remove('knit-envir.md')
 rm(env); rm(z)
