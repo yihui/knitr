9e4f6488ec9e6b9e94e19e05aa694905dd10c356
Yihui Xie <xie@yihui.name>
2013-07-03 21:56:11 -0700

    /usr/texbin should be a directory

--- R/zzz.R
+++ R/zzz.R
@@ -7,7 +7,7 @@
   }
   # add /usr/texbin to PATH for Mac OS
   if (Sys.info()['sysname'] != 'Darwin') return()
-  if (!file.exists(texbin <- '/usr/texbin')) return()
+  if (!file_test('-d', texbin <- '/usr/texbin')) return()
   path = strsplit(Sys.getenv('PATH'), ':')[[1L]]
   if (texbin %in% path) return()
   Sys.setenv(PATH = paste(c(path, texbin), collapse = ':'))
