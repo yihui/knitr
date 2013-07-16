cf5693cbccf5ed263cc855e43201fb7e3ac23c86
Yihui Xie <xie@yihui.name>
2013-07-15 17:14:48 -0700

    merge highlight-renderer.R into highlight-css.R

--- R/highlight-css.R
+++ R/highlight-css.R
@@ -78,3 +78,34 @@ css.parser = function(file, lines = readLines(file)) {
   names(styles) = dec.names
   styles
 }
+
+# styler assistant for latex
+styler_assistant_latex = function(x) {
+
+  styles = sapply(x, function(item) {
+    settings = names(item)
+    has = function(s, value) {
+      s %in% settings && grepl(value, item[[s]])
+    }
+    start = end = ''
+    if ('color' %in% settings) {
+      start = str_c(start, '\\textcolor[rgb]{', col2latexrgb(item[['color']]), '}{')
+      end = str_c(end, '}')
+    }
+    if (has('font-weight', 'bold')) {
+      start = str_c(start, '\\textbf{')
+      end = str_c('}', end)
+    }
+    if (has('font-style', 'italic')) {
+      start = str_c(start, '\\textit{')
+      end = str_c('}', end)
+    }
+    sprintf('%s#1%s', start, end)
+  })
+  sprintf('\\newcommand{\\hl%s}[1]{%s}%%', names(x), styles)
+}
+
+col2latexrgb = function(hex) {
+  col = col2rgb(hex)[, 1]/255
+  paste(round(col, 3), collapse = ',')
+}
