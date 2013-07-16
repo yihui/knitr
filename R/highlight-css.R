229721de92108af6616e058873fbbfc472a4320d
Yihui Xie <xie@yihui.name>
2013-07-15 17:19:10 -0700

    move highlight.R to highlight-css.R

--- R/highlight-css.R
+++ R/highlight-css.R
@@ -1,3 +1,12 @@
+hilight_source = function(x, format, options) {
+  if (!((format %in% c('latex', 'html')) && options$highlight))
+    return(if (options$prompt) line_prompt(x) else x)
+  res = highr::hilight(x, format, prompt = options$prompt)
+  if (format == 'html') return(res)
+  c('\\begin{alltt}', res, '\\end{alltt}')
+}
+
+
 ## stolen from Romain's highlight package (v0.3.2)
 
 # http://www.w3schools.com/css/css_colornames.asp
--- R/highlight.R
+++
@@ -1,8 +0,0 @@
-
-hilight_source = function(x, format, options) {
-  if (!((format %in% c('latex', 'html')) && options$highlight))
-    return(if (options$prompt) line_prompt(x) else x)
-  res = highr::hilight(x, format, prompt = options$prompt)
-  if (format == 'html') return(res)
-  c('\\begin{alltt}', res, '\\end{alltt}')
-}
