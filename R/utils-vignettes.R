479b8980cfacd21708245e5093427222c92a5dfd
Yihui Xie <xie@yihui.name>
2013-07-16 23:00:42 -0700

    empty markdown.HTML.header before compiling markdown vignettes (to avoid side effects)

--- R/utils-vignettes.R
+++ R/utils-vignettes.R
@@ -2,9 +2,10 @@
 
 vweave = vtangle = function(file, driver, syntax, encoding = '', quiet = FALSE, ...) {
   opts_knit$set(stop_on_error = 2L)  # should not hide errors
+  options(markdown.HTML.header = NULL)
   (if (grepl('\\.[Rr]md$', file)) knit2html else if (grepl('\\.[Rr]rst$', file)) knit2pdf else knit)(
     file, encoding = encoding, quiet = quiet, envir = globalenv()
   )
 }
 
-body(vtangle)[3L] = expression(purl(file, encoding = encoding, quiet = quiet))
+body(vtangle)[4L] = expression(purl(file, encoding = encoding, quiet = quiet))
