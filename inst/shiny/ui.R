3818a8db505e8636c649030cc04a142c238c5658
Yihui Xie <xie@yihui.name>
2013-09-27 20:34:42 -0500

    remove highlight.js and css from the shiny app; use the online version instead

--- inst/shiny/ui.R
+++ inst/shiny/ui.R
@@ -10,8 +10,10 @@ shinyUI(
       tags$script(src = 'http://ajaxorg.github.io/ace/build/src-min-noconflict/ace.js',
                   type = 'text/javascript', charset = 'utf-8'),
       tags$link(rel = 'stylesheet', type = 'text/css', href = 'assets/ace-shiny.css'),
-      tags$script(src = 'assets/highlight.pack.js', type = 'text/javascript'),
-      tags$link(rel = 'stylesheet', type = 'text/css', href = 'assets/tomorrow.css'),
+      tags$script(src = 'http://yandex.st/highlightjs/7.3/highlight.min.js', type = 'text/javascript'),
+      tags$script(src = 'http://yandex.st/highlightjs/7.3/languages/r.min.js', type = 'text/javascript'),
+      tags$link(rel = 'stylesheet', type = 'text/css',
+                href = 'http://yandex.st/highlightjs/7.3/styles/github.min.css'),
       tags$script(src = 'https://c328740.ssl.cf1.rackcdn.com/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML',
                   type = 'text/javascript')
     ),
