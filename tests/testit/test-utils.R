56a5b5dc1dfa95768e4a62eb855020b294ce5b39
Kirill Müller <kirill.mueller@ivt.baug.ethz.ch>
2013-10-24 09:39:03 +0200

    add test to support 7484fbbdfe8633578be15e

--- tests/testit/test-utils.R
+++ tests/testit/test-utils.R
@@ -30,6 +30,11 @@ assert(
 )
 
 assert(
+  'format_sci for Rnw doesn\'t add \\ensuremath{} at all',
+  !grepl('[\\]ensuremath',
+         format_sci(c(1e4, 1.2345e10, 2*pnorm(-(3:4)))))
+)
+assert(
   'format_sci for Rnw applies \\ensuremath{} correctly',
   format_sci(1e4) == '\\ensuremath{10^{4}}',
   format_sci(c(1.2345e10, 2*pnorm(-(3:4)))) ==
