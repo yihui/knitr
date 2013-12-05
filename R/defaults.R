1c9f667dade99c1fef50226493e50d2a3729917a
Kirill Müller <kirill.mueller@ivt.baug.ethz.ch>
2013-12-05 13:54:18 +0100

    also show default package options in the docs

--- R/defaults.R
+++ R/defaults.R
@@ -117,6 +117,11 @@ set_alias = function(...) {
 #' i.e. global options in base R with the prefix \code{knitr.package.}
 #' correspond to options in \code{opts_knit}. This can be useful to set package
 #' options in \file{~/.Rprofile} without loading \pkg{knitr}.
+#'
+#'  Below is a list of default package options, retrieved via
+#' \code{opts_knit$get()}:
+#'
+#' \Sexpr[results=verbatim]{str(knitr::opts_knit$get())}
 #' @references Usage: \url{http://yihui.name/knitr/objects}
 #'
 #'   A list of available options:
