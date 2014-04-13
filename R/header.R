4d9539b0697d4e89e1b75849d68e009fd33a33f6
Yihui Xie <xie@yihui.name>
2014-04-13 12:58:20 -0500

    remove the white spaces before \begin{document} when inserting the upquote package

--- R/header.R
+++ R/header.R
@@ -34,8 +34,8 @@ insert_header_latex = function(doc, b) {
     # it is safer to add usepackage{upquote} before begin{document} than after
     # documentclass{article} because it must appear after usepackage{fontenc};
     # see this weird problem: http://stackoverflow.com/q/12448507/559676
-    if (!out_format('listings') && length(j <- grep(p <- '(\\s*\\\\begin\\{document\\})', doc)[1L])) {
-      doc[j] = sub(p, '\n\\\\IfFileExists{upquote.sty}{\\\\usepackage{upquote}}{}\\1', doc[j])
+    if (!out_format('listings') && length(j <- grep(p <- '(\\s*)(\\\\begin\\{document\\})', doc)[1L])) {
+      doc[j] = sub(p, '\n\\\\IfFileExists{upquote.sty}{\\\\usepackage{upquote}}{}\n\\2', doc[j])
     }
     i = i[1L]; l = str_locate(doc[i], b)
     tmp = str_sub(doc[i], l[, 1], l[, 2])
