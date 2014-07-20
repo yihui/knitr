yihui/knitr

commit e430c60d7da595b62455a946f8b27d1dc0bd395c

even if the dev is not pdf, still try to pass dev.args to the device, e.g. pointsize is a shared argument of pdf() and png()
this fixes http://stackoverflow.com/q/21613467/559676 reported by @kbroman

R/block.R
@@ -236,7 +236,7 @@ purge_cache = function(options) {
 # not need to close the device on exit
 chunk_device = function(width, height, record = TRUE, dev, dev.args) {
   dev_new = function() {
-    if (identical(getOption('device'), pdf_null) && ('pdf' %in% dev)) {
+    if (identical(getOption('device'), pdf_null)) {
       if (!is.null(dev.args)) {
         dev.args = get_dargs(dev.args, dev, 'pdf')
         dev.args = dev.args[intersect(names(dev.args), names(formals(pdf)))]

commit bebf1178e5ebc694f932ddc3556c0d53e44a921f

fixes #791: do not pass arbitrary args to pdf(), because some may not work
e.g. family='Arial' works in cairo_pdf() but not in pdf(), although both devices have the family argument

the bug was introduced by e430c60d7da595b62455a946f8b27d1dc0bd395c

R/block.R
@@ -257,7 +257,7 @@ chunk_device = function(width, height, record = TRUE, dev, dev.args) {
     if (identical(getOption('device'), pdf_null)) {
       if (!is.null(dev.args)) {
         dev.args = get_dargs(dev.args, 'pdf')
-        dev.args = dev.args[intersect(names(dev.args), names(formals(pdf)))]
+        dev.args = dev.args[intersect(names(dev.args), c('pointsize', 'bg'))]
       }
       do.call(pdf_null, c(list(width = width, height = height), dev.args))
     } else dev.new(width = width, height = height)