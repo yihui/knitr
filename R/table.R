70e76e538f1085742832d4a8554c6dc4cb239310
Yihui Xie <xie@yihui.name>
2015-05-17 22:38:37 -0500

    do not require align to be l/r/c for LaTeX tables, e.g. you may align = c('p{2cm}', 'c', 'l')

--- R/table.R
+++ R/table.R
@@ -105,7 +105,7 @@ kable = function(
   if (!is.matrix(x)) x = matrix(x, nrow = n)
   x = gsub('^\\s*|\\s*$', '', x)
   colnames(x) = col.names
-  if (length(align) && !all(align %in% c('l', 'r', 'c')))
+  if (format != 'latex' && length(align) && !all(align %in% c('l', 'r', 'c')))
     stop("'align' must be a character vector of possible values 'l', 'r', and 'c'")
   attr(x, 'align') = align
   res = do.call(
