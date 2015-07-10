cd15c67fc48d2c5495c3b3dbba43583fb846d924
s-fiebig <s-fiebig@users.noreply.github.com>
2015-07-10 22:04:33 +0200

    Bug fix for pandoc - paragraph handling
    Pandoc creates disordered paragraph tags in the inner html for
    video/source.
    Closes yihui/knitr#1067

--- R/hooks-html.R
+++ R/hooks-html.R
@@ -87,7 +87,7 @@ hook_ffmpeg = function(x, options, format = '.webm') {
     sprintf('width="%s"', options$out.width),
     sprintf('height="%s"', options$out.height), opts
   )
-  sprintf('<video %s><source src="%s" />video of chunk %s</video>',
+  sprintf('<video %s><source src="%s" /><p>video of chunk %s</p></video>',
           opts, paste0(opts_knit$get('base.url'), mov.fname), options$label)
 }
 
