0c353b7ae4a69cbcb255df9284af9a4325a5194a
Aron Atkins <aron@gweep.net>
2015-07-24 11:18:34 -0400

    add some complex data types

--- tests/testit/test-params.R
+++ tests/testit/test-params.R
@@ -147,7 +147,11 @@ params:
   x: 1
   today: !r Sys.Date()
   posixlt: !r strptime("2015-01-01", format = "%Y-%m-%d")
+  list: [1,2,3]
+#  map: { a: 1, b: 2 }
+  map: { value: { a: 1, b: 2 } }
 ')
+# The direct map value is not supported; an explicit value field is necessary
 assert(params$x$value == 1)
 assert(identical(class(params$x$value), "integer"))
 assert(identical(params$today$expr, "Sys.Date()"))
