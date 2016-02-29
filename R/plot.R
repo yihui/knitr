e103ecd9fcbecc3593a2df4cbf2cfc90624ecca5
Yihui Xie <xie@yihui.name>
2016-02-29 16:29:05 -0600

    shorter default delays

--- R/plot.R
+++ R/plot.R
@@ -385,7 +385,7 @@ html_screenshot = function(x, options = opts_current$get(), ...) {
   wargs = options$webshot.args %n% list()
   if (is.null(wargs$vwidth)) wargs$vwidth = options$out.width.px
   if (is.null(wargs$vheight)) wargs$vheight = options$out.height.px
-  if (is.null(wargs$delay)) wargs$delay = if (i1) 1 else 3
+  if (is.null(wargs$delay)) wargs$delay = if (i1) 0.2 else 1
   d = tempfile()
   dir.create(d); on.exit(unlink(d, recursive = TRUE), add = TRUE)
   f = in_dir(d, {
