97adfea0afde9fb1f9ffdb4681ef5efa6ac8b9fb
Alex Forrence <aforren1@jhu.edu>
2016-07-14 22:20:10 -0400

    Add support for the Octave language

--- R/engine.R
+++ R/engine.R
@@ -106,8 +106,8 @@ eng_interpreted = function(options) {
     )
   } else paste(switch(
     engine, bash = '-c', coffee = '-e', groovy = '-e', lein = 'exec -e',
-    mysql = '-e', node = '-e', perl = '-e', psql = '-c', python = '-c',
-    ruby = '-e', scala = '-e', sh = '-c', zsh = '-c', NULL
+    mysql = '-e', node = '-e', octave = '--eval', perl = '-e', psql = '-c',
+    python = '-c', ruby = '-e', scala = '-e', sh = '-c', zsh = '-c', NULL
   ), shQuote(paste(options$code, collapse = '\n')))
 
   # FIXME: for these engines, the correct order is options + code + file
@@ -470,8 +470,8 @@ eng_sql = function(options) {
 local({
   for (i in c(
     'awk', 'bash', 'coffee', 'gawk', 'groovy', 'haskell', 'lein', 'mysql',
-    'node', 'perl', 'psql', 'python', 'Rscript', 'ruby', 'sas', 'scala', 'sed',
-    'sh', 'stata', 'zsh'
+    'node', 'octave', 'perl', 'psql', 'python', 'Rscript', 'ruby', 'sas',
+    'scala', 'sed', 'sh', 'stata', 'zsh'
   )) knit_engines$set(setNames(list(eng_interpreted), i))
 })
 
