5b6f791eebe961d6c2a78b11b24d2fb56dea1374
Javier Luraschi <javierluraschi@hotmail.com>
2016-08-22 11:37:04 -0700

    avoid using dbFetch in sql chunks for update queries

---
+++ tests/testit/test-sql.R
@@ -0,0 +1,51 @@
+library(testit)
+
+assert(identical(is_sql_update_query("SELECT 1"), FALSE))
+assert(identical(is_sql_update_query("SELECT * FROM foo"), FALSE))
+assert(identical(is_sql_update_query(" SELECT 1"), FALSE))
+assert(identical(is_sql_update_query("\nSELECT 1"), FALSE))
+assert(identical(is_sql_update_query("\tSELECT 1"), FALSE))
+
+assert(identical(is_sql_update_query(paste(
+  "-- Some SQL",
+  "SELECT 1",
+  sep = "\n")), FALSE))
+
+assert(identical(is_sql_update_query(paste(
+  "/* ",
+  "   Some SQL",
+  "*/",
+  "SELECT 1",
+  sep = "\n")), FALSE))
+
+assert(identical(is_sql_update_query(paste(
+  "   /* ",
+  "      Some SQL",
+  "   */",
+  "SELECT 1",
+  sep = "\n")), FALSE))
+
+assert(identical(is_sql_update_query("UPDATE foo SET a=1"), TRUE))
+assert(identical(is_sql_update_query(" UPDATE foo SET a=1"), TRUE))
+assert(identical(is_sql_update_query("\n\nUPDATE foo SET a=1"), TRUE))
+assert(identical(is_sql_update_query("\tUPDATE foo SET a=1"), TRUE))
+assert(identical(is_sql_update_query("DELETE FROM foo"), TRUE))
+assert(identical(is_sql_update_query("INSERT INTO foo values(1)"), TRUE))
+
+assert(identical(is_sql_update_query(paste(
+  "-- SELECT 1",
+  "INSERT INTO foo values(1)",
+  sep = "\n")), TRUE))
+
+assert(identical(is_sql_update_query(paste(
+  "/*SELECT 1*/",
+  "   INSERT INTO foo values(1)",
+  sep = "\n")), TRUE))
+
+assert(identical(is_sql_update_query(paste(
+  "/*",
+  "   Insert records into table",
+  "*/",
+  "",
+  "   INSERT INTO foo values(1)",
+  sep = "\n")), TRUE))
