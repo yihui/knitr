b34eb9b2cc32d7ae0df68b684ff5677f34343581
Yihui Xie <xie@yihui.name>
2016-12-05 23:12:56 -0600

    move two helper functions from bookdown

--- R/utils.R
+++ R/utils.R
@@ -901,3 +901,35 @@ restore_raw_output = function(text, chunks, markers = raw_markers) {
 raw_output = function(x, markers = raw_markers, ...) {
   asis_output(paste(c(markers[1], x, markers[2]), collapse = ''), ...)
 }
+
+# a simple JSON serializer
+tojson = function(x) {
+  if (is.null(x)) return('null')
+  if (is.logical(x)) {
+    if (length(x) != 1 || any(is.na(x)))
+      stop('Logical values of length > 1 and NA are not supported')
+    return(tolower(as.character(x)))
+  }
+  if (is.character(x) || is.numeric(x)) {
+    return(json_vector(x, length(x) != 1 || inherits(x, 'AsIs'), is.character(x)))
+  }
+  if (is.list(x)) {
+    if (length(x) == 0) return('{}')
+    return(if (is.null(names(x))) {
+      json_vector(unlist(lapply(x, tojson)), TRUE, quote = FALSE)
+    } else {
+      nms = paste0('"', names(x), '"')
+      paste0('{\n', paste(nms, unlist(lapply(x, tojson)), sep = ': ', collapse = ',\n'), '\n}')
+    })
+  }
+  stop('The class of x is not supported: ', paste(class(x), collapse = ', '))
+}
+
+json_vector = function(x, toArray = FALSE, quote = TRUE) {
+  if (quote) {
+    x = gsub('(["\\])', "\\\\\\1", x)
+    x = gsub('[[:space:]]', " ", x)
+    if (length(x)) x = paste0('"', x, '"')
+  }
+  if (toArray) paste0('[', paste(x, collapse = ', '), ']') else x
+}
