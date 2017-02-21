4e3ebc1aec7b447c9ceb6709e64e8d261763a09b
Javier Luraschi <javierluraschi@hotmail.com>
2017-02-21 10:23:54 -0800

    use knit_global instead of globalenv

--- R/engine.R
+++ R/engine.R
@@ -465,7 +465,7 @@ eng_sql = function(options) {
   # extract options
   conn = options$connection
   if (is.character(conn))
-    conn <- get(conn, envir = globalenv())
+    conn <- get(conn, envir = knit_global())
   if (is.null(conn)) stop2(
     "The 'connection' option (DBI connection) is required for sql chunks."
   )
