yihui/knitr

commit 546e05936bccf5241c39e2ec76f153499fd0bc79

Lines starting with three backticks are not inline code chunks (#655) (#1416)
* Add neg-lookback to md inline pattern (#655)

Does not flag code chunks as inline when a line starts with three backticks

* Add name and news item for PR #1416

R/pattern.R
@@ -31,7 +31,7 @@ all_patterns = list(
   `md` = list(
     chunk.begin = '^[\t >]*```+\\s*\\{([a-zA-Z0-9]+.*)\\}\\s*$',
     chunk.end = '^[\t >]*```+\\s*$',
-    ref.chunk = '^\\s*<<(.+)>>\\s*$', inline.code = '`r[ #]([^`]+)\\s*`'),
+    ref.chunk = '^\\s*<<(.+)>>\\s*$', inline.code = '(?<!(^|\n)``)`r[ #]([^`]+)\\s*`'),
 
   `rst` = list(
     chunk.begin = '^\\s*[.][.]\\s+\\{r(.*)\\}\\s*$',

DESCRIPTION
@@ -23,6 +23,7 @@ Authors@R: c(
     person("Fitch", "Simeon", role = "ctb"),
     person("Forest", "Fang", role = "ctb"),
     person(c("Frank", "E", "Harrell", "Jr"), role = "ctb", comment = "the Sweavel package at inst/misc/Sweavel.sty"),
+    person("Garrick", "Aden-Buie", role = "ctb"),
     person("Gregoire", "Detrez", role = "ctb"),
     person("Hadley", "Wickham", role = "ctb"),
     person("Heewon", "Jeon", role = "ctb"),

commit 138c634858c0eb0c697d0fb890a38788d70c90c5

a bug caused by changes in #1416: the regex all_patterns$md$inline.code should no longer be treated as base R's regex but stringi's regex, so grep() won't work any more

R/pattern.R
@@ -149,7 +149,7 @@ detect_pattern = function(text, ext) {
   for (p in names(all_patterns)) {
     for (i in c('chunk.begin', 'inline.code')) {
       pat = all_patterns[[p]][[i]]
-      if (length(pat) && length(grep(pat, text))) return(p)
+      if (length(pat) && any(stringr::str_detect(text, pat))) return(p)
     }
   }
   # *.Rtex indicates the tex syntax in knitr, but Rnw syntax in traditional

tests/testit/test-patterns.R
@@ -7,7 +7,8 @@ assert(
   identical(detect_pattern('% begin.rcode'), 'tex'),
   identical(detect_pattern('<!--begin.rcode'), 'html'),
   identical(detect_pattern('``` {r}'), 'md'),
-  identical(detect_pattern('asdf', 'rnw'), 'rnw')
+  identical(detect_pattern('asdf', 'rnw'), 'rnw'),
+  detect_pattern('foo') %==% NULL
 )
 
 assert(