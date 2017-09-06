9913b52eeeb25774db127d4ca5b3a184695818a3
Yihui Xie <xie@yihui.name>
2017-09-06 12:26:32 -0500

    spaces

--- tests/testit/test-chunkopts.R
+++ tests/testit/test-chunkopts.R
@@ -1,10 +1,10 @@
 library(testit)
 
-opts_chunk$append(list(fig.path=c('mypath/')))
+opts_chunk$append(list(fig.path = c('mypath/')))
 
 assert(
   'opts_chunk$append() returns appended chunk option',
-  identical(opts_chunk$get('fig.path'),c('figure/','mypath/'))
+  identical(opts_chunk$get('fig.path'), c('figure/', 'mypath/'))
 )
 
 opts_chunk$restore()
