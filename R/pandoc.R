0c784082b6bf8719843df74c7d984c17b6011faf
HughParsonage <hugh.parsonage@gmail.com>
2018-06-02 03:33:32 +1000

    Kll str_split

--- R/pandoc.R
+++ R/pandoc.R
@@ -126,7 +126,7 @@ pandoc_arg = function(x) {
   nms = names(x)
   if (any(grepl('\n', x))) {
     # one argument used multiple times, e.g. --bibliography
-    x = stringr::str_split(x, '\n')
+    x = strsplit(x, '\n')
     nms = rep(nms, sapply(x, length))
     x = unlist(x)
   }
