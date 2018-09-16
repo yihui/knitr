fc4282c3c2d25c5059dd393fb007e4a45f3b138b
Hiroaki Yutani <yutani.ini@gmail.com>
2018-09-16 09:43:18 +0900

    Add more tests

--- tests/testit/test-spin.R
+++ tests/testit/test-spin.R
@@ -13,8 +13,14 @@ assert(
   'spin() detects lines for documentation',
   identical(spin_w_tempfile("#' test", "1 * 1", "#' test"),
             c("test", "", "```{r }", "1 * 1", "```", "", "test")),
+  # a multiline string literal contains the pattern of doc or inline
   identical(spin_w_tempfile("code <- \"", "#' test\""),
             c("", "```{r }", "code <- \"", "#' test\"", "```", "")),
   identical(spin_w_tempfile("code <- \"", "{{ 1 + 1 }}", "\""),
-            c("", "```{r }", "code <- \"", "{{ 1 + 1 }}", "\"", "```", ""))
+            c("", "```{r }", "code <- \"", "{{ 1 + 1 }}", "\"", "```", "")),
+  # a multiline symbol contains the pattern of doc or inline
+  identical(spin_w_tempfile("`", "#' test", "`"),
+            c("", "```{r }", "`", "#' test", "`", "```", "")),
+  identical(spin_w_tempfile("`", "{{ 1 + 1 }}", "`"),
+            c("", "```{r }", "`", "{{ 1 + 1 }}", "`", "```", ""))
 )
