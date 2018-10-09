67977fdb5cc7ab817c4150ec2b51fb9bf85c8d5c
Hiroaki Yutani <yutani.ini@gmail.com>
2018-10-10 06:38:25 +0900

    Use proper number of backticks to spin() Rmd (#1611)
    * Check if doc comments are not in string literals
    
    * Add a failing test for spin() inline
    
    * Fix outdated description about inline argument of spin()
    
    * Fix spin() inline
    
    * Add more tests
    
    * Add failing tests
    
    * Use proper number of backticks to spin() Rmd
    
    * Rename confusing variable in spin.R

--- tests/testit/test-spin.R
+++ tests/testit/test-spin.R
@@ -1,16 +1,16 @@
 library(testit)
 
-spin_w_tempfile = function(...) {
+spin_w_tempfile = function(..., format = "Rmd") {
   tmp = tempfile(fileext = ".R")
   writeLines(c(...), tmp)
-  spinned = spin(tmp, knit = FALSE)
+  spinned = spin(tmp, knit = FALSE, format = format)
   result = readLines(spinned)
   file.remove(c(tmp, spinned))
   result
 }
 
 assert(
-  'spin() detects lines for documentation',
+  "spin() detects lines for documentation",
   identical(spin_w_tempfile("#' test", "1 * 1", "#' test"),
             c("test", "", "```{r }", "1 * 1", "```", "", "test")),
   # a multiline string literal contains the pattern of doc or inline
@@ -23,4 +23,14 @@ assert(
             c("", "```{r }", "`", "#' test", "`", "```", "")),
   identical(spin_w_tempfile("`", "{{ 1 + 1 }}", "`"),
             c("", "```{r }", "`", "{{ 1 + 1 }}", "`", "```", ""))
+)
+
+assert(
+  "spin() uses proper number of backticks",
+  identical(spin_w_tempfile("{{ '`' }}"),
+            c("``r  '`'  ``")),
+  identical(spin_w_tempfile("{{`x`}}"),
+            c("``r `x` ``")),
+  identical(spin_w_tempfile("x <- '", "```", "'"),
+            c("", "````{r }", "x <- '", "```", "'", "````", ""))
 )
