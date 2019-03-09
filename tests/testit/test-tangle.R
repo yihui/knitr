cc1d86183e6176289075610fcb9c4196185be06f
Yihui Xie <xie@yihui.name>
2019-03-09 09:17:17 -0600

    tweak the test

--- tests/testit/test-tangle.R
+++ tests/testit/test-tangle.R
@@ -1,14 +1,15 @@
 library(testit)
 
-tangle_text = function(text, out.format = "markdown") {
+tangle_text = function(text, out.format = 'markdown') {
   optk = opts_knit$get()
   opts_knit$set(out.format = out.format)
   on.exit(opts_knit$set(optk), add = TRUE)
-  return(knit(text = text, tangle = TRUE))
+  purl(text = text)
 }
 
-# Test that when there is no pattern specified, no pattern found, and the
-# file is tangled, knit() returns an empty string rather than the original text.
-assert(
-  identical(tangle_text("There is no code."), "")
-)
+# Test that when there is no pattern specified, no pattern found, and the file
+# is tangled, purl() returns an empty string rather than the original text.
+# https://github.com/yihui/knitr/pull/1660
+assert('when LP pattern not found in the input, purl() returns an empty string', {
+  (tangle_text('There is no code.')  %==% '')
+})
