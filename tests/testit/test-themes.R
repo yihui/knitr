f19fe42aeb77ab3069e50a100af2eb95adf00ba6
Patrick Miller <pmiller@civisanalytics.com>
2019-03-16 14:15:46 -0500

    FIX test formatting; restore opts_knit and opts_chunk

--- tests/testit/test-themes.R
+++ tests/testit/test-themes.R
@@ -2,59 +2,56 @@ library(testit)
 
 # getting and setting themes
 
-formats <- c("markdown", "latex")
-for (format in formats) {
-  opts_knit$set(out.format = format)
-  thm <- knit_theme$get('acid')
+formats = c("markdown", "latex")
+for (frm in formats) {
+  opts_knit$set(out.format = frm)
+  thm = knit_theme$get('acid')
   knit_theme$set(thm)
-  assert(
-    'knit_theme$get returns theme(s)',
-    identical(length(knit_theme$get()),
-              length(dir(system.file("themes", package = "knitr")))),
-    !is.null(thm)
-  )
+  assert('knit_theme$get returns theme(s)', {
+    (identical(length(knit_theme$get()),
+              length(dir(system.file("themes", package = "knitr")))))
+    (!is.null(thm))
+  })
 
-  assert(
-    'knit_theme$set updates highlight and background',
-    thm$highlight %==% as.character(opts_knit$get('header')[1]),
-    thm$background %==% opts_chunk$get('background')
-  )
+  assert('knit_theme$set updates highlight and background', {
+    (thm$highlight %==% as.character(opts_knit$get('header')[1]))
+    (thm$background %==% opts_chunk$get('background'))
+  })
+  opts_knit$restore()
+  opts_chunk$restore()
 }
 
 # style -> theme
 
-acid <- "\nDescription=\"Acid\"\n\nDefault        = { Colour=\"#000000\" }\nCanvas         = { Colour=\"#eeeeee\" }\nNumber         = { Colour=\"#800080\", Bold=true }\nEscape         = { Colour=\"#ff00ff\", Bold=true }\nString         = { Colour=\"#a68500\" }\nStringPreProc  = { Colour=\"#0000ff\" }\nBlockComment   = { Colour=\"#ff8000\" }\nLineComment    = { Colour=\"#f27900\" }\nPreProcessor   = { Colour=\"#0080c0\", Bold=true }\nLineNum        = { Colour=\"#303030\" }\nOperator       = { Colour=\"#ff0080\", Bold=true }\nInterpolation  = { Colour=\"#9E5DFF\" }\n\nKeywords = {\n  { Colour= \"#bb7977\", Bold=true },\n  { Colour= \"#8080c0\", Bold=true },\n  { Colour= \"#0080c0\" },\n  { Colour= \"#004466\" },\n}\n"
-fn <- tempfile(fileext = '.style')
-writeLines(acid, con = fn)
-tl <- theme2list(fn)
-assert(
-  'theme2list parsing from .style to CSS',
-  tl$Description %==% "Acid",
-  tl$Canvas$Colour %==% "#eeeeee",
-  isTRUE(tl$Number$Bold),
-  is.null(tl$Keywords)
-)
+tl = theme2list('acid.style')
+assert('theme2list parsing from .style to CSS', {
+  (tl$Description %==% "Acid")
+  (tl$Canvas$Colour %==% "#eeeeee")
+  (isTRUE(tl$Number$Bold))
+  (is.null(tl$Keywords))
+})
 
 # theme -> css
 
-css <- list2css(tl)
-assert(
-  'css is generated for a theme',
-  grepl(tl$Canvas$Colour, css[1]),
-  grepl("font-weight: bold;", css[2]),
-  grepl(tl$String$Colour, css[3])
-)
+css = list2css(tl)
+assert('css is generated for a theme', {
+  (grepl(tl$Canvas$Colour, css[1]))
+  (grepl("font-weight: bold;", css[2]))
+  (grepl(tl$String$Colour, css[3]))
+})
 
-dir.create(fdir <- file.path(tempdir(), "themes"))
-res <- lapply(1:2, function(i) {
-  fn <- paste0(fdir, "/acid", i, '.theme', collapse = "")
-  writeLines(acid, con = fn)
+fdir = file.path(tempdir(), "themes")
+dir.create(fdir)
+res = lapply(1:2, function(i) {
+  fn = paste0(fdir, "/acid", i, '.theme', collapse = "")
+  writeLines(readLines('acid.style'), con = fn)
   return()
 })
 
-dir.create(cssdir <- file.path(tempdir(), "css"))
+cssdir = file.path(tempdir(), "css")
+dir.create(cssdir)
 suppressMessages(themes2css(fdir, cssdir))
-assert(
-  length(dir(cssdir)) == 2,
-  dir(cssdir) %==% c("acid1.css", "acid2.css")
-)
+assert('themes2css creates two css files from test data', {
+  (length(dir(cssdir)) == 2)
+  (dir(cssdir) %==% c("acid1.css", "acid2.css"))
+})
