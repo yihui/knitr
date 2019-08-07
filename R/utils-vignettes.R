1a2553c8ac0578e37c8acd77189b1bd7f8f0e7d6
Yihui Xie <xie@yihui.name>
2019-08-07 14:20:56 -0500

    fix broken links

--- R/utils-vignettes.R
+++ R/utils-vignettes.R
@@ -15,7 +15,7 @@
 #'   (or \code{Sys.which('pandoc')} and \code{Sys.which('pandoc-citeproc')} in
 #'   R) to check if \command{pandoc} and \command{pandoc-citeproc} can be found.
 #'   If you use Linux, you may make symlinks to the Pandoc binaries in RStudio:
-#'   \url{https://github.com/rstudio/rmarkdown/blob/master/PANDOC.md}, or
+#'   \url{https://rmarkdown.rstudio.com/docs/articles/pandoc.html}, or
 #'   install \command{pandoc} and \command{pandoc-citeproc} separately.
 #'
 #'   When the \pkg{rmarkdown} package is not installed or not available, or
