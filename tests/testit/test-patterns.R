eb37dabecdd2f5c527e6ea5441137134f82bc60a
atusy <30277794+atusy@users.noreply.github.com>
2019-08-27 07:11:07 +0900

    update tests to reflect comments from Yihui

--- tests/testit/test-patterns.R
+++ tests/testit/test-patterns.R
@@ -7,14 +7,14 @@ assert('detect_pattern() automatically detects syntax patterns', {
   (detect_pattern('<!--begin.rcode') %==% 'html')
   (detect_pattern('``` {r}') %==% 'md')
   (detect_pattern('asdf', 'rnw') %==% 'rnw')
-  (detect_pattern('foo') %==% NULL)
+  (is.null(detect_pattern('foo')))
 })
 
 assert('group_pattern() checks if a pattern contains a group', {
-  (group_pattern('(.*)') %==% TRUE)
-  (group_pattern('()') %==% FALSE)
-  (group_pattern('abc') %==% FALSE)
-  (group_pattern(NULL) %==% FALSE)
+  (group_pattern('(.*)'))
+  (group_pattern('()'))
+  (group_pattern('abc'))
+  (!group_pattern(NULL))
 })
 
 ce_rnw = all_patterns$rnw$chunk.end
@@ -28,15 +28,15 @@ assert('patterns for Rnw', {
 
 cb_md = all_patterns$md$chunk.begin
 assert('patterns for md', {
-  # TRUE for chunk options
-  (grepl(cb_md, '```{r}') %==% TRUE)
-  (grepl(cb_md, '```{r label}') %==% TRUE)
-  (grepl(cb_md, '```{r, eval=FALSE}') %==% TRUE)
-  (grepl(cb_md, '```{awk}') %==% TRUE)
-  # FALSE for Pandoc's fenced code attributes
-  (grepl(cb_md, '```{.class}') %==% FALSE)
-  (grepl(cb_md, '```{#id}') %==% FALSE)
-  (grepl(cb_md, '```{style="color: red"}') %==% FALSE)
-  # FALSE for Pandoc's raw attribute
-  (grepl(cb_md, '```{=latex}') %==% FALSE)
+  # Chunk options
+  (grepl(cb_md, '```{r}'))
+  (grepl(cb_md, '```{r label}'))
+  (grepl(cb_md, '```{r, eval=FALSE}'))
+  (grepl(cb_md, '```{awk}'))
+  # Pandoc's fenced code attributes
+  (!grepl(cb_md, '```{.class}'))
+  (!grepl(cb_md, '```{#id}'))
+  (!grepl(cb_md, '```{style="color: red"}'))
+  # Pandoc's raw attribute
+  (!grepl(cb_md, '```{=latex}'))
 })
