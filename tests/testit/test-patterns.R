e1234a49d02d016c69ce4b2073cb118c2e99748d
atusy <30277794+atusy@users.noreply.github.com>
2019-08-27 06:55:57 +0900

    update tests

--- tests/testit/test-patterns.R
+++ tests/testit/test-patterns.R
@@ -1,43 +1,42 @@
 library(testit)
 
-assert(
-  'detect_pattern() automatically detects syntax patterns',
-  identical(detect_pattern('<<>>='), 'rnw'),
-  identical(detect_pattern('<<foo, bar=TRUE>>='), 'rnw'),
-  identical(detect_pattern('% begin.rcode'), 'tex'),
-  identical(detect_pattern('<!--begin.rcode'), 'html'),
-  identical(detect_pattern('``` {r}'), 'md'),
-  identical(detect_pattern('asdf', 'rnw'), 'rnw'),
-  detect_pattern('foo') %==% NULL
-)
+assert('detect_pattern() automatically detects syntax patterns', {
+  (detect_pattern('<<>>=') %==% 'rnw')
+  (detect_pattern('<<foo, bar=TRUE>>=') %==% 'rnw')
+  (detect_pattern('% begin.rcode') %==% 'tex')
+  (detect_pattern('<!--begin.rcode') %==% 'html')
+  (detect_pattern('``` {r}') %==% 'md')
+  (detect_pattern('asdf', 'rnw') %==% 'rnw')
+  (detect_pattern('foo') %==% NULL)
+})
 
-assert(
-  'group_pattern() checks if a pattern contains a group',
-  group_pattern('(.*)'), !group_pattern('()'), !group_pattern('abc'), !group_pattern(NULL)
-)
+assert('group_pattern() checks if a pattern contains a group', {
+  (group_pattern('(.*)') %==% TRUE)
+  (group_pattern('()') %==% FALSE)
+  (group_pattern('abc') %==% FALSE)
+  (group_pattern(NULL) %==% FALSE)
+})
 
-ce = all_patterns$rnw$chunk.end
-assert(
-  'patterns for Rnw',
-  identical(grep(ce, '  @'), 1L), # spaces before @
-  identical(grep(ce, '@  '), 1L), # spaces after @
-  identical(grep(ce, '@ %asdf'), 1L), # comments after %
-  identical(grep(ce, '@ asdf'), integer()), # only spaces/comments allowed
-  identical(grep(ce, ' @ a% sdf'), integer())
-)
+ce_rnw = all_patterns$rnw$chunk.end
+assert('patterns for Rnw', {
+  (grep(ce_rnw, '  @') %==% 1L) # spaces before @
+  (grep(ce_rnw, '@  ') %==% 1L) # spaces after @
+  (grep(ce_rnw, '@ %asdf') %==% 1L) # comments after %
+  (grep(ce_rnw, '@ asdf') %==% integer()) # only spaces/comments allowed
+  (grep(ce_rnw, ' @ a% sdf') %==% integer())
+})
 
 cb_md = all_patterns$md$chunk.begin
-assert(
-  'patterns for md',
-  # Are they chunk options?
-  isTRUE(grepl(cb_md, '```{r}')),
-  isTRUE(grepl(cb_md, '```{r label}')),
-  isTRUE(grepl(cb_md, '```{r, eval=FALSE}')),
-  isTRUE(grepl(cb_md, '```{awk}')),
-  # Are they Pandoc's fenced code attributes?
-  !isTRUE(grepl(cb_md, '```{.class}')),
-  !isTRUE(grepl(cb_md, '```{#id}')),
-  !isTRUE(grepl(cb_md, '```{style="color: red"}')),
-  # Is it Pandoc's raw attribute?
-  !isTRUE(grepl(cb_md, '```{=latex}'))
-)
+assert('patterns for md', {
+  # TRUE for chunk options
+  (grepl(cb_md, '```{r}') %==% TRUE)
+  (grepl(cb_md, '```{r label}') %==% TRUE)
+  (grepl(cb_md, '```{r, eval=FALSE}') %==% TRUE)
+  (grepl(cb_md, '```{awk}') %==% TRUE)
+  # FALSE for Pandoc's fenced code attributes
+  (grepl(cb_md, '```{.class}') %==% FALSE)
+  (grepl(cb_md, '```{#id}') %==% FALSE)
+  (grepl(cb_md, '```{style="color: red"}') %==% FALSE)
+  # FALSE for Pandoc's raw attribute
+  (grepl(cb_md, '```{=latex}') %==% FALSE)
+})
