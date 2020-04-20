84daff31480742abf7e59552c1e36e01010db694
Yihui Xie <xie@yihui.name>
2020-04-20 11:05:24 -0500

    suggest ragg (amend #1834)

--- R/plot.R
+++ R/plot.R
@@ -112,7 +112,7 @@ save_plot = function(plot, name, dev, width, height, ext, dpi, options) {
 
     # similar to load_device(), but the `dpi` argument is named `res`
     ragg_png = function(...) {
-      getFromNamespace('agg_png', 'ragg')(..., res = dpi, units = 'in')
+      ragg::agg_png(..., res = dpi, units = 'in')
     },
 
     tikz = function(...) {
