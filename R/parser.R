yihui/knitr

commit c081ef9c07bda8718479dd2821cc16a7b9eaf405

preserve the engine in params.src for quarto

DESCRIPTION
@@ -1,7 +1,7 @@
 Package: knitr
 Type: Package
 Title: A General-Purpose Package for Dynamic Report Generation in R
-Version: 1.34.2
+Version: 1.34.3
 Authors@R: c(
     person("Yihui", "Xie", role = c("aut", "cre"), email = "xie@yihui.name", comment = c(ORCID = "0000-0003-0645-5666")),
     person("Abhraneel", "Sarma", role = "ctb"),

R/parser.R
@@ -88,7 +88,8 @@ parse_block = function(code, header, params.src, markdown_mode = out_format('mar
     params = gsub('^\\s*,\\s*', '', params)
   }
 
-  params.src = params
+  # for quarto, preserve the actual original params.src and do not remove the engine
+  if (!(is_quarto <- !is.null(opts_knit$get('quarto.version')))) params.src = params
   params = parse_params(params.src)
 
   # merge with possible chunk options written as (YAML or CSV) metadata in
@@ -131,7 +132,7 @@ parse_block = function(code, header, params.src, markdown_mode = out_format('mar
   }
 
   # for quarto only
-  if (!is.null(opts_knit$get('quarto.version'))) {
+  if (is_quarto) {
     params$original.params.src = params.src
     params$chunk.echo = isTRUE(params[['echo']])
     params$yaml.code = parts$src

commit b4ee20c911f584c9ef9e246e24ffa1e4c2de2a67

fix the regression introduced by c081ef9c07bda8718479dd2821cc16a7b9eaf405: should parse params instead of params.src

DESCRIPTION
@@ -1,7 +1,7 @@
 Package: knitr
 Type: Package
 Title: A General-Purpose Package for Dynamic Report Generation in R
-Version: 1.34.3
+Version: 1.34.4
 Authors@R: c(
     person("Yihui", "Xie", role = c("aut", "cre"), email = "xie@yihui.name", comment = c(ORCID = "0000-0003-0645-5666")),
     person("Abhraneel", "Sarma", role = "ctb"),

R/parser.R
@@ -90,7 +90,7 @@ parse_block = function(code, header, params.src, markdown_mode = out_format('mar
 
   # for quarto, preserve the actual original params.src and do not remove the engine
   if (!(is_quarto <- !is.null(opts_knit$get('quarto.version')))) params.src = params
-  params = parse_params(params.src)
+  params = parse_params(params)
 
   # merge with possible chunk options written as (YAML or CSV) metadata in
   # chunk, and remove metadata from code body