df295d94515bc7c6a6105d2c518dea34f40345d3
Yihui Xie <xie@yihui.name>
2022-03-23 16:25:52 -0500

    I guess this test should work on all platforms

--- tests/testit/test-plot.R
+++ tests/testit/test-plot.R
@@ -139,11 +139,11 @@ if (xfun::loadable('tikzDevice') &&
 # https://github.com/yihui/knitr/issues/1166
 knit(text = "\\Sexpr{include_graphics('myfigure.pdf', error = FALSE)}", quiet = TRUE)
 
-assert('include_graphics warns on absolute paths and fixes them', {
+assert('include_graphics() warns on absolute paths and expands ~', {
   path1 = "~/test.png"
   (has_warning(include_graphics(path1, error = FALSE)))
   (!has_warning(include_graphics("img/test.png", error = FALSE)))
-  # (unclass(include_graphics(path1, error = FALSE)) %==% path.expand(path1))
+  (unclass(suppressWarnings(include_graphics(path1, error = FALSE))) %==% path.expand(path1))
 })
 
 with_par = function(expr, ...) {
