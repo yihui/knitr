d47ac0e366608006fea5ed114de4b817dd4d4206
Yihui Xie <xie@yihui.name>
2022-04-07 23:16:08 -0500

    it's hard to come with an example that makes xfun::relative_path() fail to convert absolute paths on *nix, so just remove this test

--- tests/testit/test-plot.R
+++ tests/testit/test-plot.R
@@ -139,9 +139,8 @@ if (xfun::loadable('tikzDevice') &&
 # https://github.com/yihui/knitr/issues/1166
 knit(text = "\\Sexpr{include_graphics('myfigure.pdf', error = FALSE)}", quiet = TRUE)
 
-assert('include_graphics() warns on absolute paths and expands ~', {
+assert('include_graphics() expands ~', {
   path1 = "~/test.png"
-  (has_warning(include_graphics(path1, error = FALSE)))
   (!has_warning(include_graphics("img/test.png", error = FALSE)))
   (unclass(suppressWarnings(include_graphics(path1, error = FALSE))) %==% path.expand(path1))
 })
