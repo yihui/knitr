d8bd33e63d470a51fef0daf70be2a2fed235a51d
Yihui Xie <xie@yihui.name>
2022-07-12 16:01:58 -0500

    fewer backslashes

--- R/utils.R
+++ R/utils.R
@@ -1063,5 +1063,5 @@ image_uri = function(f) xfun::base64_uri(f)
 # Change all "[label](url)" to "label" except leave "`[label](url)`" as is
 remove_urls = function(x) {
   # regex adapted from https://dev.to/mattkenefick/regex-convert-markdown-links-to-html-anchors-f7j
-  gsub("(?<!`)\\[([^\\]]+)\\]\\(([^\\)]+)\\)(?!`)", "\\1", x, perl = TRUE)
+  gsub("(?<!`)\\[([^]]+)\\]\\(([^)]+)\\)(?!`)", "\\1", x, perl = TRUE)
 }
