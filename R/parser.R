b999afb309bb3489867b47166f3e4b3ffb2a87ef
Yihui Xie <xie@yihui.name>
2022-07-25 23:04:31 -0500

    yaml is a required package of knitr

--- R/parser.R
+++ R/parser.R
@@ -807,9 +807,6 @@ convert_chunk_header = function(
 ) {
 
   type = match.arg(type)
-  if (type == "yaml" && !xfun::pkg_available("yaml"))
-    stop("Converting to yaml format require the yaml R package.")
-
   # extract fenced header information
   text = xfun::read_utf8(input)
   ext  = xfun::file_ext(input)
