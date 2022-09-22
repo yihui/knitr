44cb92aad1f7356d772e6a7fe304761b29869928
Richard Iannone <riannone@me.com>
2022-09-22 10:15:39 -0400

    Add perl arg to `str_complete_replace()`

--- R/utils-string_formatting.R
+++ R/utils-string_formatting.R
@@ -1,3 +1,3 @@
-str_complete_replace = function(string, pattern, replacement) {
-  gsub(pattern, replacement, string, perl = TRUE)
+str_complete_replace = function(string, pattern, replacement, perl = TRUE) {
+  gsub(pattern, replacement, string, perl = perl)
 }
