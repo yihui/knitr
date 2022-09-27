0b7eb4ef29fab871924a93839e7931294a5604e0
Yihui Xie <xie@yihui.name>
2022-09-27 09:51:48 -0500

    showWarnings = FALSE is unnecessary (xfun::dir_create() won't create a dir if it exists)

--- R/block.R
+++ R/block.R
@@ -93,7 +93,7 @@ call_block = function(block) {
     }
     hash = paste(valid_path(params$cache.path, label), digest(content), sep = '_')
     params$hash = hash
-    xfun::dir_create(dirname(hash), showWarnings = FALSE)
+    xfun::dir_create(dirname(hash))
     if (cache_exists(params) &&
         isFALSE(params$cache.rebuild) &&
         params$engine != 'Rcpp') {
