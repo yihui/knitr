0f8e7bed95071fd3da73180891960c8ceeae48f2
Richard Iannone <riannone@me.com>
2022-12-22 14:41:47 -0500

    Replace use of str_match_all() with base equivalent

--- R/parser.R
+++ R/parser.R
@@ -357,7 +357,7 @@ parse_inline = function(input, patterns) {
   loc = cbind(start = numeric(0), end = numeric(0))
   if (group_pattern(inline.code)) loc = str_complete_locate(input, inline.code)[[1]]
   if (nrow(loc)) {
-    code = stringr::str_match_all(input, inline.code)[[1L]]
+    code = str_get_all_matches(input, inline.code)[[1L]]
     code = if (NCOL(code) >= 2L) {
       code[is.na(code)] = ''
       apply(code[, -1L, drop = FALSE], 1, paste, collapse = '')
