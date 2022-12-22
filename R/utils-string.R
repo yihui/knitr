260509c9a63e25f3ff582b3b794da48e09c094ec
Richard Iannone <riannone@me.com>
2022-12-22 15:01:41 -0500

    Adapt gregexpr() to custom grepexec_knitr() fn

--- R/utils-string.R
+++ R/utils-string.R
@@ -47,7 +47,7 @@ str_complete_extract = function(string, pattern) {
 
 # replacement for stringr::str_match_all()
 str_get_all_matches = function(string, pattern) {
-  res = gregexec(pattern, string, perl = TRUE)
+  res = gregexec_knitr(pattern, string, perl = TRUE)
   loc = lapply(res, FUN = function(x) {
     if (all(is.na(x))) return(list(NA_character_))
     match_attr = attr(x, "match.length")
@@ -94,6 +94,54 @@ str_get_all_matches = function(string, pattern) {
     elem
   })
   out
+}
+
+gregexec_knitr = function(pattern, text, perl = FALSE) {
+  if (is.factor(text) && length(levels(text)) < length(text)) {
+    out = gregexec_knitr(pattern, c(levels(text), NA_character_), perl)
+    outna = out[length(out)]
+    out = out[text]
+    out[is.na(text)] = outna
+    return(out)
+  }
+  dat = gregexpr(pattern = pattern, text = text, perl = perl)
+  if (perl) {
+    capt.attr = c('capture.start', 'capture.length', 'capture.names')
+    process = function(x) {
+      if (anyNA(x) || any(x < 0)) y = x
+      else {
+        y = t(cbind(x, attr(x, "capture.start")))
+        attributes(y)[names(attributes(x))] = attributes(x)
+        ml = t(cbind(attr(x, "match.length"), attr(x, "capture.length")))
+        nm = attr(x, 'capture.names')
+        dimnames(ml) = dimnames(y) = if (any(nzchar(nm))) list(c("", nm), NULL)
+        attr(y, "match.length") = ml
+        y
+      }
+      attributes(y)[capt.attr] = NULL
+      y
+    }
+    lapply(dat, process)
+  } else {
+    m1 = lapply(regmatches(text, dat), regexec, pattern = pattern, perl = perl)
+    mlen = lengths(m1)
+    res = vector("list", length(m1))
+    im = mlen > 0
+    res[!im] = dat[!im]
+    res[im] = Map(
+      function(outer, inner) {
+        tmp = do.call(cbind, inner)
+        attributes(tmp)[names(attributes(inner))] = attributes(inner)
+        attr(tmp, 'match.length') = do.call(cbind, lapply(inner, `attr`, 'match.length'))
+        attr(tmp, 'useBytes') = attr(outer, 'useBytes')
+        attr(tmp, 'index.type') = attr(outer, 'index.type')
+        tmp + rep(outer - 1L, each = nrow(tmp))
+      },
+      dat[im],
+      m1[im]
+    )
+    res
+  }
 }
 
 # replacement for stringr::str_sub() and used internally in other string
