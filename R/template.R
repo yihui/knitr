13ba95181eb275bd735c532308d7192bf27d8df1
Richard Iannone <riannone@me.com>
2022-12-22 09:33:19 -0500

    Replace uses of str_extract_all()

--- R/template.R
+++ R/template.R
@@ -120,7 +120,7 @@ knit_expand = function(file, ..., text = read_utf8(file), delim = c('{{', '}}') 
   txt = one_string(text)
   loc = stringr::str_locate_all(txt, delim)[[1L]]
   if (nrow(loc) == 0L) return(txt) # no match
-  mat = stringr::str_extract_all(txt, delim)[[1L]]
+  mat = str_complete_extract(txt, delim)[[1L]]
   mat = sub(delim, '\\1', mat)
   env = list(...)
   env = if (length(env)) list2env(env, parent = parent.frame()) else parent.frame()
