f6c069b9862d6d2be0ac025e48e98a5019cb3663
dmurdoch <murdoch.duncan@gmail.com>
2023-01-18 18:12:56 -0500

    Allow concordances to be embedded in HTML/Markdown output  (#2200)

--- R/concordance.R
+++ R/concordance.R
@@ -7,8 +7,7 @@ knit_concord = new_defaults(list(
 
 # do not consider child mode for concordance
 concord_mode = function() {
-  opts_knit$get('concordance') && !child_mode() &&
-    out_format(c('latex', 'sweave', 'listings'))
+  opts_knit$get('concordance') && !child_mode()
 }
 
 current_lines = function(i) {
@@ -42,8 +41,15 @@ concord_gen = function(infile, outfile) {
   vals = c(1L, as.numeric(rbind(vals$lengths, vals$values)))
   concordance = paste(strwrap(paste(vals, collapse = ' ')), collapse = ' %\n')
 
-  confile = paste(sans_ext(outfile), 'concordance.tex', sep = '-')
   # write to file
-  cat('\\Sconcordance{concordance:', outfile, ':', infile, ':%\n',
+  if (is_html_output()) {
+    cat(
+      '<!-- concordance:', outfile, ':', infile, ':', concordance, ' -->\n',
+      sep = '', file = outfile, append = TRUE
+    )
+  } else if (is_latex_output()) {
+    confile = paste(sans_ext(outfile), 'concordance.tex', sep = '-')
+    cat('\\Sconcordance{concordance:', outfile, ':', infile, ':%\n',
       concordance, '}\n', sep = '', file = confile)
+  }
 }
