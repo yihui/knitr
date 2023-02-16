78f1db5c32e59c85e06f9704ab7b22a459c2b945
Yihui Xie <xie@yihui.name>
2023-02-16 13:49:10 -0600

    out_format() could be empty, too (when running outside knit())

--- R/utils.R
+++ R/utils.R
@@ -383,7 +383,8 @@ is_latex_output = function() {
 #' @rdname output_type
 #' @export
 is_html_output = function(fmt = pandoc_to(), excludes = NULL) {
-  if (length(fmt) == 0) fmt = out_format()
+  fmt = fmt %n% out_format()
+  if (length(fmt) == 0) return(FALSE)
   if (grepl('^markdown', fmt)) fmt = 'markdown'
   if (fmt == 'epub3') fmt = 'epub'
   fmts = c('markdown', 'epub', 'epub2', 'html', 'html4', 'html5', 'revealjs', 's5', 'slideous', 'slidy', 'gfm')
