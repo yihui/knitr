bc29a3d78015873617db7c559d953b39037572d7
Yihui Xie <xie@yihui.name>
2023-09-19 11:48:58 -0500

    simplify this clumsy decade-old JS code

--- inst/misc/toggleR.js
+++ inst/misc/toggleR.js
@@ -1,16 +1,9 @@
 // toggle visibility of R source blocks in R Markdown output
-function toggle_R() {
-  var x = document.getElementsByClassName('r');
-  if (x.length == 0) return;
-  function toggle_vis(o) {
-    var d = o.style.display;
-    o.style.display = (d == 'block' || d == '') ? 'none':'block';
-  }
-
-  for (i = 0; i < x.length; i++) {
-    var y = x[i];
-    if (y.tagName.toLowerCase() === 'pre') toggle_vis(y);
-  }
-}
-
-document.write('<button type="button" onclick="toggle_R();" style="position: absolute; top: 0; right: 0;">Hide/Show Source</button>')
+(d => {
+  d.insertAdjacentHTML('beforeend','<button style="position:absolute;top:0;right:0;z-index:2;">Toggle Source</button>');
+  d.lastElementChild.onclick = (e) => {
+    d.querySelectorAll('pre.r').forEach(el => {
+      el.style.display = (el.style.display === 'none') ? 'block' : 'none';
+    });
+  };
+})(document.body);
