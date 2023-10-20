04a31f2fd08cf2dc286bd2e5be927d76aa093da0
Yihui Xie <xie@yihui.name>
2023-10-20 14:17:23 -0500

    preserve integers

--- R/parser.R
+++ R/parser.R
@@ -866,12 +866,12 @@ convert_chunk_header = function(
             x
           },
           # use character with verbatim for no quotes
-          # so that numeric as kept unchanged (without changing precision)
+          # so that integers as kept unchanged (without changing precision)
           # fig.width = 10, should not be fig-width: 10.0
           numeric = function(x) {
-            x = as.character(x)
-            class(x) = 'verbatim'
-            x
+            if (length(x) != 1) return(x)
+            x2 = as.integer(x)
+            if (x2 == x) x2 else x
           }), line.sep = '\n'), '\n')[[1]]
       params3 = paste0(prefix, params3)
     }
