401f8a9f651b96d3c10dc04cba15ff9cace4a2a0
Christophe Dervieux <christophe.dervieux@gmail.com>
2024-12-17 11:55:24 +0100

    set `rlang_trace_top_env` options to get cleaner trace back
    This option will be used by rlang::entrace() to find the top of the trace to use.
    
    This is one currently in rlang calling knitr::knit_global() but it makes more sense to have it in knitr directly

--- R/output.R
+++ R/output.R
@@ -309,11 +309,14 @@ process_file = function(text, output) {
     group = groups[[i]]
     knit_concord$set(block = i)
     res[i] = xfun:::handle_error(
-      withCallingHandlers(
-        if (tangle) process_tangle(group) else process_group(group),
-        error = function(e) {
-          if (progress && is.function(pb$interrupt)) pb$interrupt()
-        }
+      with_options(
+  	    withCallingHandlers(
+          if (tangle) process_tangle(group) else process_group(group),
+          error = function(e) {
+            if (progress && is.function(pb$interrupt)) pb$interrupt()
+          }
+        ),
+        list(rlang_trace_top_env = knit_global())
       ),
       function(loc) {
         setwd(wd)
