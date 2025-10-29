84b314e66ba90d6aa437fa4d3e948dda68aa6343
shikokuchuo <53399081+shikokuchuo@users.noreply.github.com>
2025-10-29 19:12:45 +0000

    Add more tests

--- tests/testit/test-otel.R
+++ tests/testit/test-otel.R
@@ -4,6 +4,7 @@ if (requireNamespace("otelsdk", quietly = TRUE)) {
 
   record = otelsdk::with_otel_record({
     knitr:::otel_refresh_tracer("knitr")
+
     knit(
       text = c('<<tidy=FALSE, eval=1:2, echo=FALSE, results="asis">>=', '1', '1+', '1', '1', '@'),
       quiet = TRUE
@@ -12,7 +13,7 @@ if (requireNamespace("otelsdk", quietly = TRUE)) {
 
   traces = record$traces
 
-  assert('otel tracing works', {
+  assert('otel tracing works on text input', {
     (length(traces) %==% 3L)
     (startsWith(traces[[1L]]$name, 'knitr processing'))
     (traces[[1L]]$attributes$knitr.format %==% 'latex')
@@ -27,6 +28,37 @@ if (requireNamespace("otelsdk", quietly = TRUE)) {
     (traces[[3L]]$attributes$knitr.format %==% 'latex')
     (traces[[3L]]$attributes$knitr.input %==% '')
     (traces[[3L]]$attributes$knitr.output %==% '')
+  })
+
+  record = otelsdk::with_otel_record({
+    knitr:::otel_refresh_tracer("knitr")
+
+    local({
+      env = new.env()
+      env$y = 1:3
+      z = 5
+      on.exit(file.remove('knit-envir.md'))
+      knit('knit-envir.Rmd', envir = env, quiet = TRUE)
+    })
+  })
+
+  traces = record$traces
+
+  assert('otel tracing works when knitting files', {
+    (length(traces) %==% 3L)
+    (traces[[1L]]$name %==% 'knitr processing knit-envir.Rmd')
+    (traces[[1L]]$attributes$knitr.format %==% 'markdown')
+    (traces[[1L]]$attributes$knitr.input %==% 'knit-envir.Rmd')
+    (traces[[1L]]$attributes$knitr.output %==% 'knit-envir.md')
+    (traces[[2L]]$name %==% 'knit test')
+    (traces[[2L]]$attributes$knitr.chunk.device %==% 'png')
+    (traces[[2L]]$attributes$knitr.chunk.echo %==% TRUE)
+    (traces[[2L]]$attributes$knitr.chunk.engine %==% 'R')
+    (traces[[2L]]$attributes$knitr.chunk.eval %==% TRUE)
+    (traces[[3L]]$name %==% 'knitr output knit-envir.md')
+    (traces[[3L]]$attributes$knitr.format %==% 'markdown')
+    (traces[[3L]]$attributes$knitr.input %==% 'knit-envir.Rmd')
+    (traces[[3L]]$attributes$knitr.output %==% 'knit-envir.md')
   })
 
 }
